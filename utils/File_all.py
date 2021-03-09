e19778db4d6dfb139522b4262dc8c66420105de9
wuyenlin <finne0831@gmail.com>
2021-03-09 14:46:24 +0100

    added save_frame

--- utils/File_all.py
+++ utils/File_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import os, json, csv
+import os, json, csv, sys
 from numpy.linalg import norm
 import numpy as np 
 from tqdm import tqdm
@@ -23,6 +23,7 @@ class All(File):
         except FileExistsError:
             pass
         while (cap.isOpened()):
+            print("processing S{} Seq{} video_{}".format(self.S, self.Se, self.vid))
             for k in tqdm(range(int(cap.get(cv.CAP_PROP_FRAME_COUNT)))):
                 ret, frame = cap.read()
                 if ret:
@@ -51,5 +52,24 @@ class All(File):
         np.savez_compressed("dataset/S{}/Seq{}/imageSequence/video_{}".format(self.S,self.Se,self.vid), data)
         cap.release()
 
+# char_list = [sys.argv[1], sys.argv[2]]
+def save_frame(char_list):
+    for char in char_list:
+        for seq in (1,2):
+            for vid in [0,1,2,4,5,6,7,8]:
+                v = All(char, seq, vid)
+                v.save_cropped(True, True)
+
+def merge_npz():
+    merge_data = []
+    for k in [0,1,2,4,5,6,7,8]:
+        npz = "dataset/S1/Seq1/imageSequence/video_{}.npz".format(k)
+        t = np.load(npz, allow_pickle=True)
+        t = t['arr_0'].reshape(1,-1)
+        merge_data.append(*t)
+    np.savez_compressed("dataset/S1/Seq1/imageSequence/S1seq1", merge_data)
+    print("saved")
+
+
 if __name__ == "__main__": 
-    pass
+    merge_npz()
