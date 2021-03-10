d6d75132a35eff6271bdd34a647a84d8b965035f
wuyenlin <finne0831@gmail.com>
2021-03-10 12:31:38 +0100

    fixed typo and added 2dkpts

--- utils/File.py
+++ utils/File.py
@@ -6,7 +6,7 @@ import numpy as np
 from tqdm import tqdm
 from Video import *
 
-class All(File):
+class All(Video):
     def __init__(self, S, Se, vid):
         super().__init__(S, Se, vid)
 
@@ -31,7 +31,6 @@ class All(File):
 
     def in_box(self, pt, start, end):
         return True if (start[0]<=pt[0]<=end[0] and start[1]<=pt[1]<=end[1]) else False
-
 
     def check_valid(self, nframe, start, end):
         """
@@ -80,29 +79,19 @@ class All(File):
 
                         if save_txt:
                             data[k]["directory"] = filename
-                            data[k]["keypoints"] = self.objPoint.reshape(1,-1)
+                            data[k]["2d_keypoints"] = self.imgPoint.reshape(1,-1)
+                            data[k]["3d_keypoints"] = self.objPoint.reshape(1,-1)
+
             break
         np.savez_compressed("dataset/S{}/Seq{}/imageSequence/video_{}".format(self.S,self.Se,self.vid), data)
         cap.release()
 
-# char_list = [sys.argv[1], sys.argv[2]]
-def save_frame(char_list):
-    for char in char_list:
-        for seq in (1,2):
-            for vid in [0,1,2,4,5,6,7,8]:
-                v = All(char, seq, vid)
-                v.save_cropped(True, True)
-
-def merge_npz():
-    merge_data = []
-    for k in [0,1,2,4,5,6,7,8]:
-        npz = "dataset/S1/Seq1/imageSequence/video_{}.npz".format(k)
-        t = np.load(npz, allow_pickle=True)
-        t = t['arr_0'].reshape(1,-1)
-        merge_data.append(*t)
-    np.savez_compressed("dataset/S1/Seq1/imageSequence/S1seq1", merge_data)
-    print("saved")
-
+def save_frame(char):
+    for seq in (1,2):
+        for vid in [0,1,2,4,5,6,7,8]:
+            v = All(char, seq, vid)
+            v.save_cropped(False, True)
 
 if __name__ == "__main__": 
-    merge_npz()
+    char_list = sys.argv[1]
+    save_frame(char_list)
