9a434ba0a24762a52df81a3e1f129b6b7cb4a44e
wuyenlin <finne0831@gmail.com>
2021-03-11 18:04:33 +0100

    added full option

--- merge.py
+++ merge.py
@@ -2,18 +2,23 @@ import os, json, csv, sys
 from numpy.linalg import norm
 import numpy as np 
 from tqdm import tqdm
-from File import *
 
 
-def merge_npz():
+def merge_npz(full=False):
     merge_data = []
     for k in [0,1,2,4,5,6,7,8]:
-        npz = "dataset/S1/Seq1/imageSequence/video_{}.npz".format(k)
+        if full:
+            npz = "dataset/S1/Seq1/imageSequence/full_video_{}.npz".format(k)
+        else:
+            npz = "dataset/S1/Seq1/imageSequence/video_{}.npz".format(k)
         t = np.load(npz, allow_pickle=True)
         t = t['arr_0'].reshape(1,-1)
         merge_data.append(*t)
-    np.savez_compressed("dataset/S1/Seq1/imageSequence/S1seq1", merge_data)
+    if full:
+        np.savez_compressed("dataset/S1/Seq1/imageSequence/full_S1seq1", merge_data)
+    else:
+        np.savez_compressed("dataset/S1/Seq1/imageSequence/S1seq1", merge_data)
     print("saved")
 
 if __name__ == "__main__":
-    merge_npz()
+    merge_npz(True)
