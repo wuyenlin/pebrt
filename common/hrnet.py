bfc3f88564d89913ac030614d12ab4e10fed6044
wuyenlin <finne0831@gmail.com>
2021-03-12 10:59:16 +0100

    rearrange COCO joints to match MPI

--- common/hrnet.py
+++ common/hrnet.py
@@ -276,9 +276,42 @@ def normalize_screen_coordinates(X, w, h):
     # Normalize so that [0, w] is mapped to [-1, 1], while preserving the aspect ratio
     return X/w*2 - [1, h/w]
 
-def get_joints(heatmap):
+
+def coco_mpi(coco_joints):
     """
-    turn input heatmap (bs,17,64,48) into coordinates of 17 joints
+    convert predicted COCO joints to MPI-INF-3DHP joints order
+    input is a (17,2) numpy array of COCO joints order
+    returns a (17,2) numpy array of MPI-INF-3DHP order
+    """
+    mpi_joints = np.zeros_like(coco_joints)
+    mpi_joints[4,:] = coco_joints[0,:]
+    # arms
+    mpi_joints[5,:] = coco_joints[6,:]
+    mpi_joints[6,:] = coco_joints[8,:]
+    mpi_joints[7,:] = coco_joints[10,:]
+    mpi_joints[8,:] = coco_joints[5,:]
+    mpi_joints[9,:] = coco_joints[7,:]
+    mpi_joints[10,:] = coco_joints[9,:]
+    # legs
+    mpi_joints[11,:] = coco_joints[12,:]
+    mpi_joints[12,:] = coco_joints[14,:]
+    mpi_joints[13,:] = coco_joints[16,:]
+    mpi_joints[14,:] = coco_joints[11,:]
+    mpi_joints[15,:] = coco_joints[13,:]
+    mpi_joints[16,:] = coco_joints[15,:]
+    # spine
+    mpi_joints[0,:] = (coco_joints[5,:] + coco_joints[6,:])/2
+    mpi_joints[2,:] = (coco_joints[11,:] + coco_joints[12,:])/2
+    mpi_joints[1,:] = (mpi_joints[0,:] + mpi_joints[2,:])/2
+    mpi_joints[3,:] = (mpi_joints[0,:] + mpi_joints[4,:])/2
+
+    return mpi_joints
+
+
+def hmap_joints(heatmap):
+    """
+    turn input heatmap (bs,17,h,w) into coordinates of 17 joints
+    return tensor of (17,2) joints on x,y coordinates for a bs
     """
     assert heatmap.shape[1:] == (17,56,56), "{}".format(heatmap.shape)
     bs = heatmap.size(0)
@@ -288,21 +321,23 @@ def get_joints(heatmap):
         for j, joint in enumerate(human):
             pt = np.unravel_index(np.argmax(joint), (56,56))
             joints_2d[i,j,:] = np.asarray(pt)
-        # joints_2d[i,:,:] = normalize_screen_coordinates(joints_2d[i,:,:], 56, 56)
+        joints_2d[i,:,:] = coco_mpi(joints_2d[i,:,:])
+        joints_2d[i,:,:] = normalize_screen_coordinates(joints_2d[i,:,:], 56, 56)
     assert joints_2d.shape == (bs,17,2), "{}".format(joints_2d.shape)
     joints_2d[:,:,[0,1]] = joints_2d[:,:,[1,0]]
     return torch.Tensor(joints_2d)
 
-
 if __name__ == '__main__':
     transforms = transforms.Compose([
         transforms.Resize([256,192]),
+        # transforms.Resize([224,224]),
         transforms.ToTensor(),  
         transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5]),
     ]) 
     model = HRNet(32, 17, 0.1)
     model.load_state_dict(torch.load('./weights/pose_hrnet_w32_256x192.pth'))
-    img = Image.open("dataset/S1/Seq1/imageSequence/video_8/frame006139.jpg")
+    # model.load_state_dict(torch.load('./finetune_epoch_50.bin'))
+    img = Image.open("dataset/S1/Seq1/imageSequence/video_8/frame006192.jpg")
     img = transforms(img).cuda()
     img = img.unsqueeze(0)
 
@@ -323,7 +358,7 @@ if __name__ == '__main__':
     out_cpu = output.cpu().detach().numpy()
     plt.figure(1)
     plt.subplot(131)
-    plt.imshow(Image.open("dataset/S1/Seq1/imageSequence/video_8/frame006139.jpg"))
+    plt.imshow(Image.open("dataset/S1/Seq1/imageSequence/video_8/frame006192.jpg"))
     plt.figure(1)
     plt.subplot(132)
     plt.imshow(out_cpu)
@@ -331,5 +366,7 @@ if __name__ == '__main__':
     plt.figure(1)
     plt.subplot(133)
     plt.scatter(joints_2d[:,1], joints_2d[:,0])
+    plt.xlim(0,56)
+    plt.ylim(0,56)
     plt.gca().invert_yaxis()
     plt.show()
