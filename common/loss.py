cbf0b240f61586d0ff882ee38a4b86691c56cef3
wuyenlin <finne0831@gmail.com>
2021-03-16 21:08:59 +0100

    cleaned up and added comments

--- common/loss.py
+++ common/loss.py
@@ -1,18 +1,14 @@
-# Copyright (c) 2018-present, Facebook, Inc.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-#
-
 import math
 import torch
 import numpy as np
+
 
 def mpjpe(predicted, target):
     """
     Mean per-joint position error (i.e. mean Euclidean distance),
     often referred to as "Protocol #1" in many papers.
+    Borrowed from 'facebookresearch/VideoPose3D'.
+    (https://github.com/facebookresearch/VideoPose3D/blob/master/common/loss.py)
     """
     assert predicted.shape == target.shape, "{}, {}".format(predicted.shape, target.shape)
     if torch.cuda.is_available():
@@ -20,23 +16,27 @@ def mpjpe(predicted, target):
         target = target.cuda()
     return torch.mean(torch.norm(predicted - target, dim=len(target.shape)-1))
 
-def punish(predicted, bone1, bone2, weights):
 
-# the commented code is an easier explanation of this function, 
+def punish(predicted, bone1, bone2, weights):
+    """
+    This function verifies that symmetric bones have the same length.
+    If the predicted skeleton shows different bone lengths on a pair of bones,
+    one of their respective joints will be weighted 2 (instead of 1) in loss function.
+    """
+
+# the commented part is an easier explanation of this function, 
 # but training becomes slower as bs increases
 
     # for bs in range(predicted.shape[0]):
-    #     len_bone1 = torch.norm(predicted[bs, bone1[0], :]-predicted[bs, bone2[1], :])
+    #     len_bone1 = torch.norm(predicted[bs, bone1[0], :]-predicted[bs, bone1[1], :])
     #     len_bone2 = torch.norm(predicted[bs, bone2[0], :]-predicted[bs, bone2[1], :])
     #     if not math.isclose(len_bone1, len_bone2, abs_tol=1e-02):
     #         weights[bs, bone1[0]] = 2
     #         weights[bs, bone2[0]] = 2
-    
-    # return weights
 
     len1 = torch.norm(predicted[:, bone1[0], :]-predicted[:, bone1[1], :], dim=1)
     len2 = torch.norm(predicted[:, bone2[0], :]-predicted[:, bone2[1], :], dim=1)
-    diff = abs(len2-len1) > 1e-02
+    diff = abs(len2-len1) > 0.1
     diff = list(map(int, diff))
     diff = torch.FloatTensor(diff) + 1
     weights[:,bone1[0]] = diff
@@ -46,6 +46,11 @@ def punish(predicted, bone1, bone2, weights):
 
 
 def anth_mpjpe(predicted, target):
+    """
+    Added own implementation of weighted MPJPE by ensuring:
+    1. symmetric bones have the same length
+    2. wrists are correctly detected
+    """
     assert predicted.shape == target.shape, "{}, {}".format(predicted.shape, target.shape)
     w = torch.ones(predicted.shape[0], predicted.shape[1]) # (64,17)
     
@@ -56,15 +61,15 @@ def anth_mpjpe(predicted, target):
 
     for bone in bones:
         w = punish(predicted, bone[0], bone[1], w)
-    assert w.shape[0] == predicted.shape[0], "{},{}".format(w.shape, predicted.shape)
+    w[:,7] = 2
+    w[:,10] = 2
 
     if torch.cuda.is_available():
         predicted = predicted.cuda()
         target = target.cuda()
         w = w.cuda()
 
-    anth_mpjpe = torch.mean(w * torch.norm(predicted - target, dim=len(target.shape)-1))
-    return anth_mpjpe
+    return torch.mean(w * torch.norm(predicted - target, dim=len(target.shape)-1))
 
 
 if __name__ == "__main__":
