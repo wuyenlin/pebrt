b294916590a661dc910c360c8eca6b91afa9f3d7
wuyenlin <finne0831@gmail.com>
2021-03-16 15:35:36 +0100

    added my loss func (constraints on bone lengths)

--- common/loss.py
+++ common/loss.py
@@ -5,6 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 #
 
+import math
 import torch
 import numpy as np
 
@@ -18,78 +19,52 @@ def mpjpe(predicted, target):
         predicted = predicted.cuda()
         target = target.cuda()
     return torch.mean(torch.norm(predicted - target, dim=len(target.shape)-1))
-    
-def weighted_mpjpe(predicted, target, w):
-    """
-    Weighted mean per-joint position error (i.e. mean Euclidean distance)
-    """
-    assert predicted.shape == target.shape
-    assert w.shape[0] == predicted.shape[0]
-    return torch.mean(w * torch.norm(predicted - target, dim=len(target.shape)-1))
 
-def p_mpjpe(predicted, target):
-    """
-    Pose error: MPJPE after rigid alignment (scale, rotation, and translation),
-    often referred to as "Protocol #2" in many papers.
-    """
-    assert predicted.shape == target.shape
-    
-    muX = np.mean(target, axis=1, keepdims=True)
-    muY = np.mean(predicted, axis=1, keepdims=True)
-    
-    X0 = target - muX
-    Y0 = predicted - muY
+def punish(predicted, bone1, bone2, weights):
 
-    normX = np.sqrt(np.sum(X0**2, axis=(1, 2), keepdims=True))
-    normY = np.sqrt(np.sum(Y0**2, axis=(1, 2), keepdims=True))
-    
-    X0 /= normX
-    Y0 /= normY
+# the commented code is an easier explanation of this function, 
+# but training becomes slower as bs increases
 
-    H = np.matmul(X0.transpose(0, 2, 1), Y0)
-    U, s, Vt = np.linalg.svd(H)
-    V = Vt.transpose(0, 2, 1)
-    R = np.matmul(V, U.transpose(0, 2, 1))
+    # for bs in range(predicted.shape[0]):
+    #     len_bone1 = torch.norm(predicted[bs, bone1[0], :]-predicted[bs, bone2[1], :])
+    #     len_bone2 = torch.norm(predicted[bs, bone2[0], :]-predicted[bs, bone2[1], :])
+    #     if not math.isclose(len_bone1, len_bone2, abs_tol=1e-02):
+    #         weights[bs, bone1[0]] = 2
+    #         weights[bs, bone2[0]] = 2
+    
+    # return weights
 
-    # Avoid improper rotations (reflections), i.e. rotations with det(R) = -1
-    sign_detR = np.sign(np.expand_dims(np.linalg.det(R), axis=1))
-    V[:, :, -1] *= sign_detR
-    s[:, -1] *= sign_detR.flatten()
-    R = np.matmul(V, U.transpose(0, 2, 1)) # Rotation
+    len1 = torch.norm(predicted[:, bone1[0], :]-predicted[:, bone1[1], :], dim=1)
+    len2 = torch.norm(predicted[:, bone2[0], :]-predicted[:, bone2[1], :], dim=1)
+    diff = abs(len2-len1) > 1e-02
+    diff = list(map(int, diff))
+    diff = torch.FloatTensor(diff) + 1
+    weights[:,bone1[0]] = diff
+    weights[:,bone2[0]] = diff
 
-    tr = np.expand_dims(np.sum(s, axis=1, keepdims=True), axis=2)
+    return weights
 
-    a = tr * normX / normY # Scale
-    t = muX - a*np.matmul(muY, R) # Translation
-    
-    # Perform rigid transformation on the input
-    predicted_aligned = a*np.matmul(predicted, R) + t
-    
-    # Return MPJPE
-    return np.mean(np.linalg.norm(predicted_aligned - target, axis=len(target.shape)-1))
-    
-def n_mpjpe(predicted, target):
-    """
-    Normalized MPJPE (scale only), adapted from:
-    https://github.com/hrhodin/UnsupervisedGeometryAwareRepresentationLearning/blob/master/losses/poses.py
-    """
-    assert predicted.shape == target.shape
-    
-    norm_predicted = torch.mean(torch.sum(predicted**2, dim=3, keepdim=True), dim=2, keepdim=True)
-    norm_target = torch.mean(torch.sum(target*predicted, dim=3, keepdim=True), dim=2, keepdim=True)
-    scale = norm_target / norm_predicted
-    return mpjpe(scale * predicted, target)
 
-def mean_velocity_error(predicted, target):
-    """
-    Mean per-joint velocity error (i.e. mean Euclidean distance of the 1st derivative)
-    """
-    assert predicted.shape == target.shape
+def anth_mpjpe(predicted, target):
+    assert predicted.shape == target.shape, "{}, {}".format(predicted.shape, target.shape)
+    w = torch.ones(predicted.shape[0], predicted.shape[1]) # (64,17)
     
-    velocity_predicted = np.diff(predicted, axis=0)
-    velocity_target = np.diff(target, axis=0)
-    
-    return np.mean(np.linalg.norm(velocity_predicted - velocity_target, axis=len(target.shape)-1))
+    bones = (((5,6),(8,9)),
+             ((6,7),(9,10)),
+             ((11,12),(14,15)),
+             ((12,13),(15,16)))
+
+    for bone in bones:
+        w = punish(predicted, bone[0], bone[1], w)
+    assert w.shape[0] == predicted.shape[0], "{},{}".format(w.shape, predicted.shape)
+
+    if torch.cuda.is_available():
+        predicted = predicted.cuda()
+        target = target.cuda()
+        w = w.cuda()
+
+    anth_mpjpe = torch.mean(w * torch.norm(predicted - target, dim=len(target.shape)-1))
+    return anth_mpjpe
 
 
 if __name__ == "__main__":
