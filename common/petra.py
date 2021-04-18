19c5c949834878ab6cb46734a43ab062ecb1cdf7
wuyenlin <finne0831@gmail.com>
2021-04-18 22:09:33 +0800

    added petra and angle

---
+++ common/petra.py
@@ -0,0 +1,114 @@
+import os, math
+import numpy as np
+import torch, torchvision
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+try:
+    from common.hrnet import *
+    from common.embed import *
+except ModuleNotFoundError:
+    from hrnet import *
+    from embed import *
+
+
+class TransformerEncoder(nn.Module):
+    """
+    Pose Estimation with Transformer
+    """
+    def __init__(self, d_model=34, nhead=2, num_layers=6, 
+                    num_joints_in=17, num_joints_out=17,
+                    num_patches=256, lift=True):
+        super().__init__()
+
+        self.lift = lift
+        if lift:
+            print("INFO: Using default positional encoder")
+            self.pe = PositionalEncoder(d_model)
+            self.tanh = nn.Tanh()
+        encoder_layer = nn.TransformerEncoderLayer(d_model, nhead)
+        self.transformer = nn.TransformerEncoder(encoder_layer, num_layers)
+        self.lin_out = nn.Linear(d_model, 30)
+
+        self.d_model = d_model
+        self.nhead = nhead
+        self.num_joints_in = num_joints_in
+        self.num_joints_out = num_joints_out 
+
+
+    def forward(self, x):
+        bs = x.size(0)
+        x = x.flatten(1).unsqueeze(1) #(bs,1,30)
+        x = self.pe(x)
+        x = self.transformer(x)
+        x = self.lin_out(x).squeeze(1)
+
+        return x
+
+
+class PETRA(nn.Module):
+    """
+    PETRA - Pose Estimation using TRansformer outputing Angles
+    """
+    def __init__(self, device):
+        super().__init__()
+        
+        self.lift = lift
+        self.device = device
+        self.backbone = HRNet(32, 17, 0.1)
+        pretrained_weight = "../weights/pose_hrnet_w32_256x192.pth"
+        self.backbone.load_state_dict(torch.load(pretrained_weight))
+        print("INFO: Pre-trained weights of HRNet loaded from {}".format(pretrained_weight))
+        self.transformer = TransformerEncoder(num_layers=8)
+
+
+    def _decode_joints(self, heatmap, device):
+        """
+        turn input heatmap (bs,17,h,w) into coordinates of 17 joints
+        return tensor of (17,2) joints on x,y coordinates for a batch
+        """
+        assert heatmap.shape[1:] == (17,64,64), "{}".format(heatmap.shape)
+        bs = heatmap.size(0)
+        joints_2d = np.zeros([bs,17,2])
+        heatmap = heatmap.cpu().detach().numpy()
+
+        for i, human in enumerate(heatmap):
+            for j, joint in enumerate(human):
+                pt = np.unravel_index(np.argmax(joint), (64,64))
+                joints_2d[i,j,:] = np.asarray(pt, dtype=np.float64)
+            joints_2d[i,:,:] = coco_mpi(joints_2d[i,:,:])
+            # reposition root joint at origin
+            joints_2d[i,:,:] = joints_2d[i,:,:] - joints_2d[i,2,:]
+        assert joints_2d.shape == (bs,17,2), "{}".format(joints_2d.shape)
+        # np.unravel_index gives (y,x) coordinates. need to swap it to (x,y)
+        joints_2d[:,:,[0,1]] = joints_2d[:,:,[1,0]]
+        return torch.tensor(joints_2d, device=device)
+
+
+    def forward(self, x):
+        if self.lift:
+            x = self.backbone(x)
+            x = self._decode_joints(x, self.device)
+            out_x = self.transformer(x.float())
+
+        return x, out_x
+
+
+if __name__ == "__main__":
+    from torchvision import transforms
+    from PIL import Image
+
+    transforms = transforms.Compose([
+        transforms.Resize([256,256]),
+        transforms.ToTensor(),  
+        transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5]),
+    ]) 
+    model = PETRA(device="cuda:0")
+    model = model.cuda()
+    img = Image.open("dataset/S1/Seq1/imageSequence/video_8/frame006192.jpg")
+    img = transforms(img)
+    img = img.unsqueeze(0)
+    print(img.shape)
+    img = img.cuda()
+    _, output = model(img)
+    print(output.shape)
