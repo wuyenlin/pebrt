c65b51a67ec4477029020e0a1c1ea2e7cf1e997d
wuyenlin <finne0831@gmail.com>
2021-04-19 10:25:22 +0800

    cleaned plot

--- plot.py
+++ plot.py
@@ -4,6 +4,7 @@ from torchvision import transforms
 from PIL import Image
 import matplotlib.pyplot as plt
 from common.petr import PETR
+from common.petra import PETRA
 
 
 transforms = transforms.Compose([
@@ -14,8 +15,8 @@ transforms = transforms.Compose([
 
 
 def att():
-    model = PETR(lift=True)
-    model.load_state_dict(torch.load('./new_checkpoint/ft_5.bin')['model'])
+    model = PETR(device="cuda:0",lift=True)
+    model.load_state_dict(torch.load('./anth_checkpoint/ft_5.bin')['model'])
     model = model.cuda()
     model.eval()
 
@@ -30,41 +31,44 @@ def att():
 def viz(bones):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = PETR(device, lift=True)
-    # model.load_state_dict(torch.load('./checkpoint/0321.bin'))
-    model.load_state_dict(torch.load('./new_checkpoint/ft_5.bin')['model'])
+    # model.load_state_dict(torch.load('./anth_checkpoint/ft_5.bin')['model'])
+    model.load_state_dict(torch.load('./checkpoint/ft_4.bin')['model'])
     model = model.cuda()
     model.eval()
 
-    img_list = [
-            # "dataset/S1/Seq1/imageSequence/video_4/frame001049.jpg",
-            # "dataset/S1/Seq1/imageSequence/video_5/frame001182.jpg",
-            # "dataset/S1/Seq1/imageSequence/video_8/frame000049.jpg",
-            # "dataset/S1/Seq1/imageSequence/video_2/frame002453.jpg",
+    imgs = [
+        #0
+            ["dataset/S1/Seq1/imageSequence/video_4/frame001049.jpg",
+            "dataset/S1/Seq1/imageSequence/video_5/frame001182.jpg",
+            "dataset/S1/Seq1/imageSequence/video_8/frame000049.jpg",
+            "dataset/S1/Seq1/imageSequence/video_2/frame002453.jpg"],
 
-            # "dataset/S1/Seq1/imageSequence/video_1/frame000684.jpg",
-            # "dataset/S1/Seq1/imageSequence/video_1/frame000232.jpg",
-            # "dataset/S1/Seq2/imageSequence/video_0/frame006958.jpg",
-            # "dataset/S1/Seq2/imageSequence/video_5/frame008665.jpg",
+        #1
+            ["dataset/S1/Seq1/imageSequence/video_1/frame000684.jpg",
+            "dataset/S1/Seq1/imageSequence/video_1/frame000232.jpg",
+            "dataset/S1/Seq2/imageSequence/video_0/frame006958.jpg",
+            "dataset/S1/Seq2/imageSequence/video_5/frame008665.jpg"],
 
-            # "dataset/S4/Seq1/imageSequence/video_5/frame000283.jpg",
-            # "dataset/S4/Seq1/imageSequence/video_7/frame002168.jpg",
-            # "dataset/S7/Seq1/imageSequence/video_8/frame001549.jpg",
-            # "dataset/S8/Seq1/imageSequence/video_0/frame005071.jpg",
+        #2
+            ["dataset/S4/Seq1/imageSequence/video_5/frame000283.jpg",
+            "dataset/S4/Seq1/imageSequence/video_7/frame002168.jpg",
+            "dataset/S7/Seq1/imageSequence/video_8/frame001549.jpg",
+            "dataset/S8/Seq1/imageSequence/video_0/frame005071.jpg"],
 
-            # "dataset/S5/Seq2/imageSequence/video_7/frame004884.jpg",
-            # "dataset/S6/Seq2/imageSequence/video_6/frame002665.jpg",
-            # "dataset/S7/Seq2/imageSequence/video_4/frame001103.jpg",
-            # "dataset/S8/Seq2/imageSequence/video_1/frame001069.jpg", #bad
+        #3
+            ["dataset/S5/Seq2/imageSequence/video_7/frame004884.jpg",
+            "dataset/S6/Seq2/imageSequence/video_6/frame002665.jpg",
+            "dataset/S7/Seq2/imageSequence/video_4/frame001103.jpg",
+            "dataset/S8/Seq2/imageSequence/video_1/frame001069.jpg"], #bad
 
-            "dataset/S5/Seq1/imageSequence/video_1/frame002604.jpg",
+        #4
+            ["dataset/S5/Seq1/imageSequence/video_1/frame002604.jpg",
             "dataset/S5/Seq2/imageSequence/video_8/frame000659.jpg",
             "dataset/S6/Seq1/imageSequence/video_7/frame003115.jpg",
-            "dataset/S6/Seq1/imageSequence/video_8/frame005868.jpg",
+            "dataset/S6/Seq1/imageSequence/video_8/frame005868.jpg"]
 
-            # "h36.jpg"
-            # "dataset/S1/Seq1/imageSequence/full_video_2/frame000002.jpg",
-            # "../human36m/human36M/protocol_1/S5/Directions_2/imageSequence/58860488/img_000501.jpg"
             ]
+    img_list = imgs[3]
     k = 1
     fig = plt.figure()
     for i in range(len(img_list)):
