c8d7cece4092724f68be620d94c93d134cb21492
wuyenlin <finne0831@gmail.com>
2021-04-22 16:35:52 +0800

    added joint collision

--- common/loss.py
+++ common/loss.py
@@ -1,6 +1,8 @@
-import math
+import math, cmath
+from math import sin, cos
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def mpjpe(predicted, target):
@@ -21,9 +23,10 @@ def p_mpjpe(predicted, target):
     """
     Pose error: MPJPE after rigid alignment (scale, rotation, and translation),
     often referred to as "Protocol #2" in many papers.
+    Borrowed from 'facebookresearch/VideoPose3D'.
+    (https://github.com/facebookresearch/VideoPose3D/blob/master/common/loss.py)
     """
     assert predicted.shape == target.shape
-    
     muX = np.mean(target, axis=1, keepdims=True)
     muY = np.mean(predicted, axis=1, keepdims=True)
     
@@ -59,23 +62,15 @@ def p_mpjpe(predicted, target):
     return np.mean(np.linalg.norm(predicted_aligned - target, axis=len(target.shape)-1))
     
 
-def mean_velocity_error(predicted, target):
-    """
-    Mean per-joint velocity error (i.e. mean Euclidean distance of the 1st derivative)
-    """
-    assert predicted.shape == target.shape
-    
-    velocity_predicted = np.diff(predicted, axis=0)
-    velocity_target = np.diff(target, axis=0)
-    
-    return np.mean(np.linalg.norm(velocity_predicted - velocity_target, axis=len(target.shape)-1))
 
-
-def punish(predicted, bone1, bone2, weights):
+def punish(predicted, bone1, bone2, weights, thres=0.1):
     """
     This function verifies that symmetric bones have the same length.
     If the predicted skeleton shows different bone lengths on a pair of bones,
     one of their respective joints will be weighted 2 (instead of 1) in loss function.
+    # TODO: add bone collision (see if 2 lines/volumes have intersection)
+
+    ::return:: a weight matrix of shape (bs, 17)
     """
 
 # the commented part is an easier explanation of this function, 
@@ -90,13 +85,22 @@ def punish(predicted, bone1, bone2, weights):
 
     len1 = torch.norm(predicted[:, bone1[0], :]-predicted[:, bone1[1], :], dim=1)
     len2 = torch.norm(predicted[:, bone2[0], :]-predicted[:, bone2[1], :], dim=1)
-    diff = abs(len2-len1) > 0.1
-    diff = list(map(int, diff))
-    diff = torch.FloatTensor(diff) + 1
+    diff = abs(len2-len1) > thres
+    diff = diff.double() + 1
     weights[:,bone1[0]], weights[:,bone1[1]] = diff, diff
     weights[:,bone2[0]], weights[:,bone2[1]] = diff, diff
 
     return weights
+
+
+def joint_collision(predicted, target, weight, thres=0.1):
+    bs = predicted.shape[0]
+
+    diff = torch.linalg.norm(predicted - target, dim=2) > thres
+    diff = diff.double() + 1
+    weight *= diff
+
+    return weight
 
 
 def anth_mpjpe(predicted, target):
@@ -104,20 +108,26 @@ def anth_mpjpe(predicted, target):
     Added own implementation of weighted MPJPE by ensuring:
     1. symmetric bones have the same length
     2. wrists and ankles are correctly detected
+    3. joint collision/occlusion are punished
     """
     assert predicted.shape == target.shape, "{}, {}".format(predicted.shape, target.shape)
-    w = torch.ones(predicted.shape[0], predicted.shape[1]) # (64,17)
+    bs = predicted.shape[0]
+    w = torch.ones(bs, predicted.shape[1]) # (bs,17)
     
     bones = (((5,6),(8,9)),
              ((6,7),(9,10)),
              ((11,12),(14,15)),
              ((12,13),(15,16)))
 
+    # 1. symmetric bones have same length, else punish
     for bone in bones:
         w = punish(predicted, bone[0], bone[1], w)
 
-    # focus on correct prediction of wrists and ankles
+    # 2. focus on correct prediction of wrists and ankles
     w[:,7], w[:,10], w[:,13], w[:,16] = 2, 2, 2, 2
+
+    # 3. joint collision
+    w = joint_collision(predicted, target, w)
 
     if torch.cuda.is_available():
         predicted = predicted.cuda()
@@ -127,11 +137,7 @@ def anth_mpjpe(predicted, target):
     return torch.mean(w * torch.norm(predicted - target, dim=len(target.shape)-1))
 
 
-def human_model(H):
-    """Implementation of Winter human model"""
-    pass
-
 if __name__ == "__main__":
-    a = torch.rand([16,17,2])
-    b = torch.rand([16,17,2])
-    print(mpjpe(a,b))
+    a = torch.rand([4,17,3])
+    b = torch.rand([4,17,3])
+    print(anth_mpjpe(a,b))
