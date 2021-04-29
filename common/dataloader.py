5a70fdf6973f0e759c76f02cd5871d71cdb60db9
wuyenlin <finne0831@gmail.com>
2021-04-29 09:39:32 +0800

    add 2d and 3d subplot

--- common/dataloader.py
+++ common/dataloader.py
@@ -1,21 +1,15 @@
-import os
-import cv2 as cv
-import torch
-import torchvision
 from torchvision import transforms
 import torchvision.transforms.functional as F
 from torch.utils.data import DataLoader
 import numpy as np
 from numpy import random
 import matplotlib.pyplot as plt
+import cv2 as cv
 from PIL import Image, ImageEnhance, ImageFilter
-
-
-def imshow(img):
-    img = img / 2 + 0.5
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+try:
+    from common.human import *
+except ModuleNotFoundError:
+    from human import *
 
 
 def collate_fn(batch):
@@ -27,10 +21,10 @@ class AddGaussianNoise(object):
     def __init__(self, mean=0., std=1.):
         self.std = std
         self.mean = mean
-
+        
     def __call__(self, tensor):
         return tensor + torch.randn(tensor.size()) * self.std + self.mean
-
+    
     def __repr__(self):
         return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
 
@@ -53,12 +47,13 @@ class Data:
         for vid in vid_list:
             for frame in data[vid].keys():
                 pts_2d = (data[vid][frame]['2d_keypoints']).reshape(-1,2)
-                pro_pts_2d = self.zero_center(self.pop_joints(pts_2d)*96/2048)
+                pro_pts_2d = self.zero_center(self.pop_joints(pts_2d))
                 self.gt_pts2d.append(torch.from_numpy(pro_pts_2d))
 
                 pts_3d = (data[vid][frame]['3d_keypoints']).reshape(-1,3)
                 cam_3d = self.to_camera_coordinate(pts_2d, pts_3d, vid)
-                self.gt_pts3d.append(torch.from_numpy(self.zero_center(cam_3d)/1000))
+                gt_3d = self.zero_center(cam_3d)/1000
+                self.gt_pts3d.append(gt_3d)
                 self.img_path.append(data[vid][frame]['directory'])
 
     def __getitem__(self, index):
@@ -74,12 +69,14 @@ class Data:
 
     def __len__(self):
         return len(self.img_path)
-
+    
 
     def pop_joints(self, kpts):
-        '''
-        Get 17 joints from the original 28
-        '''
+        """
+        Get 17 joints from the original 28 
+        :param kpts: orginal kpts from MPI-INF-3DHP (an array of (28,3))
+        :return new_skel: an array of (17,3)
+        """
         new_skel = np.zeros([17,3]) if kpts.shape[-1]==3 else np.zeros([17,2])
         ext_list = [0,2,4,5,6,         # spine+head
                     9,10,11,14,15,16,  # arms
@@ -92,6 +89,8 @@ class Data:
     def get_intrinsic(self, camera):
         """
         Parse camera matrix from calibration file
+        :param camera:              camera number (used in MPI dataset)
+        :return intrinsic matrix:
         """
         calib = open("./dataset/S1/Seq1/camera.calibration","r")
         content = calib.readlines()
@@ -110,27 +109,30 @@ class Data:
         assert ret
         R = cv.Rodrigues(R)[0]
         E = np.concatenate((R,t), axis=1)  # [R|t], a 3x4 matrix
-
+    
         pts_3d = cv.convertPointsToHomogeneous(self.pop_joints(pts_3d)).transpose().squeeze(1)
         cam_coor = E @ pts_3d
         cam_3d = cam_coor.transpose()
         return cam_3d
 
-
+    
     def zero_center(self, cam):
+        """
+        translate root joint to origin (0,0,0)
+        """
         return cam - cam[2,:]
 
 
-def test():
+def try_load():
     train_npz = "dataset/S1/Seq1/imageSequence/S1.npz"
     train_dataset = Data(train_npz, transforms, True)
-    print(len(train_dataset))
-    trainloader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=16, drop_last=True)
+    trainloader = DataLoader(train_dataset, batch_size=4, 
+                        shuffle=True, num_workers=2, drop_last=True)
     print("data loaded!")
     dataiter = iter(trainloader)
     img_path, images, kpts, labels = dataiter.next()
-    # imshow(torchvision.utils.make_grid(images))
-
+    print(labels[0])
+    
     bones = (
     (0,1), (0,3), (1,2), (3,4),  # spine + head
     (0,5), (0,8),
@@ -139,24 +141,40 @@ def test():
     (11,12), (12,13), (14,15), (15,16), # legs
     )
 
-    pts = labels[0]
     fig = plt.figure()
-    ax = fig.add_subplot(121)
+    ax = fig.add_subplot(131)
     plt.imshow(Image.open(img_path[0]))
-    ax = fig.add_subplot(122, projection='3d')
+
+    # 2nd - 2D Pose
+    pts = kpts[0]
+    ax = fig.add_subplot(132)
+    ax.scatter(pts[:,0], pts[:,1])
+    for bone in bones:
+        xS = (pts[bone[0],0], pts[bone[1],0])
+        yS = (pts[bone[0],1], pts[bone[1],1])
+        
+        ax.plot(xS, yS)
+    ax.invert_yaxis()
+
+    # 3rd - 3D Pose
+    pts = labels[0]
+    ax = fig.add_subplot(133, projection='3d')
     ax.scatter(pts[:,0], pts[:,1], pts[:,2])
     for bone in bones:
         xS = (pts[bone[0],0], pts[bone[1],0])
         yS = (pts[bone[0],1], pts[bone[1],1])
         zS = (pts[bone[0],2], pts[bone[1],2])
-
+        
         ax.plot(xS, yS, zS)
+    
     ax.view_init(elev=-80, azim=-90)
     plt.xlim(-1,1)
     plt.ylim(-1,1)
+    ax.set_zlim(-1,1)
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
+
     plt.show()
 
 
@@ -167,14 +185,4 @@ if __name__ == "__main__":
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5]),
     ])
-    # test()
-    rmat_z = np.array([[0,-1,0],[1,0,0],[0,0,1]])
-    rmat_x = np.array([[1,0,0],[0,0,-1],[0,1,0]])
-    rot = rmat_z @ rmat_x
-    angles, mtxR, mtxQ, Qx, Qy, Qz = cv.RQDecomp3x3(rot)
-    print(rot)
-    print("\n")
-    print(angles)
-    print(Qx)
-    print(Qy)
-    print(Qz)
+    try_load()
