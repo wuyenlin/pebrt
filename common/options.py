1c2dbe52cc1c783fc285770f913333a021bc5799
wuyenlin <finne0831@gmail.com>
2021-05-20 12:04:56 +0800

    ready to finetune

--- common/options.py
+++ common/options.py
@@ -6,10 +6,10 @@ def args_parser():
     
     # Hyperparameters
     parser.add_argument('--start_epoch', type=int, default=0)
-    parser.add_argument('--epoch', type=int, default=51)
+    parser.add_argument('--epoch', type=int, default=31)
     parser.add_argument('--bs', type=int, default=2)
-    parser.add_argument('--lr', type=float, default=1e-04)
-    parser.add_argument('--lr_backbone', type=float, default=0)
+    parser.add_argument('--lr', type=float, default=1e-06)
+    parser.add_argument('--lr_backbone', type=float, default=1e-06)
     parser.add_argument('--weight_decay', type=float, default=1e-05)
     parser.add_argument('--lr_drop', default=10, type=int)
 
@@ -22,7 +22,7 @@ def args_parser():
     parser.add_argument('--eval', action='store_true')
     parser.add_argument('--lift', action='store_true', help='using lifting model')
     parser.add_argument('--export_training_curves', action='store_true', help="Save train/val curves in .png file")
-    parser.add_argument('--dataset', type=str, default="./dataset/S1/Seq1/imageSequence/S1.npz")
+    parser.add_argument('--dataset', type=str, default="./dataset/S3/Seq1/imageSequence/S3.npz")
     parser.add_argument('--device', default="cuda", help="device used")
     parser.add_argument('--resume', type=str, default=None, help="Loading model checkpoint")
     parser.add_argument('--distributed', action='store_true')
