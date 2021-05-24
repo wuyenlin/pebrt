878004bb79b758a16d46312c6ff935653853c4e9
wuyenlin <finne0831@gmail.com>
2021-05-24 14:39:15 +0800

    cleaned up code

--- plot.py
+++ plot.py
@@ -23,6 +23,22 @@ bones = (
 (11,12), (12,13), (14,15), (15,16), # legs
 )
 
+def plot3d(ax, bones, output):
+    for p in output:
+        ax.scatter(p[0], p[1], p[2], c='r', alpha=0.7)
+
+    for index in bones:
+        xS = (output[index[0]][0],output[index[1]][0])
+        yS = (output[index[0]][1],output[index[1]][1])
+        zS = (output[index[0]][2],output[index[1]][2])
+        ax.plot(xS, yS, zS)
+    ax.view_init(elev=-80, azim=-90)
+    ax.autoscale()
+    ax.set_zlim(-1,1)
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+
 
 def viz(savefig=False):
     train_npz = "dataset/S1/Seq1/imageSequence/S1Seq1.npz"
@@ -40,7 +56,6 @@ def viz(savefig=False):
     net.eval()
 
 
-    # 1st row
     fig = plt.figure()
     for k in range(1,5):
         ax = fig.add_subplot(2, 4, k)
@@ -48,7 +63,6 @@ def viz(savefig=False):
 
         h = Human(1.8, "cpu")
         pts = kpt_2d[k-1]
-        # pts = torch.tensor(pts)
         pts = torch.tensor(pts.unsqueeze(0)).cuda()
         output = net(pts)
 
@@ -56,22 +70,9 @@ def viz(savefig=False):
         output = output.detach().numpy()
 
         ax = fig.add_subplot(2, 4, k+4, projection='3d')
-        for p in output:
-            ax.scatter(p[0], p[1], p[2], c='r')
-
-        for index in bones:
-            xS = (output[index[0]][0],output[index[1]][0])
-            yS = (output[index[0]][1],output[index[1]][1])
-            zS = (output[index[0]][2],output[index[1]][2])
-            ax.plot(xS, yS, zS)
-        ax.view_init(elev=-80, azim=-90)
-        ax.autoscale()
+        plot3d(ax, bones, output)
         plt.xlim(-1,1)
         plt.ylim(-1,1)
-        ax.set_zlim(-1,1)
-        ax.set_xlabel("X")
-        ax.set_ylabel("Y")
-        ax.set_zlabel("Z")
 
     plt.show()
     if savefig:
