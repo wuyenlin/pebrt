a37e320fc8f120c3a2ea6122d376c6b58527e4c5
wuyenlin <finne0831@gmail.com>
2021-05-27 22:19:17 +0800

    rm arg lift

--- common/options.py
+++ common/options.py
@@ -8,7 +8,7 @@ def args_parser():
     parser.add_argument('--start_epoch', type=int, default=0)
     parser.add_argument('--epoch', type=int, default=51)
     parser.add_argument('--bs', type=int, default=2)
-    parser.add_argument('--lr', type=float, default=1e-04)
+    parser.add_argument('--lr', type=float, default=2e-04)
     parser.add_argument('--lr_backbone', type=float, default=0)
     parser.add_argument('--weight_decay', type=float, default=1e-05)
     parser.add_argument('--lr_drop', default=10, type=int)
@@ -20,7 +20,6 @@ def args_parser():
     # dataset
     parser.add_argument('--num_workers', default=1, type=int)
     parser.add_argument('--eval', action='store_true')
-    parser.add_argument('--lift', action='store_true', help='using lifting model')
     parser.add_argument('--export_training_curves', action='store_true', help="Save train/val curves in .png file")
     parser.add_argument('--dataset', type=str, default="./dataset/S1/Seq1/imageSequence/S1.npz")
     parser.add_argument('--device', default="cuda", help="device used")
