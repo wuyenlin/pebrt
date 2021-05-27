1ee511d6cf4a7d0f49b49c9ba2e8f919fa624e08
wuyenlin <finne0831@gmail.com>
2021-05-27 10:11:12 +0800

    rm end-to-end model

--- common/petr.py
+++ common/petr.py
@@ -1,8 +1,6 @@
-import os, math
 import numpy as np
-import torch, torchvision
+import torch
 import torch.nn as nn
-import matplotlib.pyplot as plt
 
 try:
     from common.hrnet import *
@@ -17,18 +15,12 @@ class TransformerEncoder(nn.Module):
     Pose Estimation with Transformer
     """
     def __init__(self, d_model=34, nhead=2, num_layers=6, 
-                    num_joints_in=17, num_joints_out=17,
-                    num_patches=256, lift=True):
+                    num_joints_in=17, num_joints_out=17):
         super().__init__()
 
-        self.lift = lift
-        if lift:
-            print("INFO: Using default positional encoder")
-            self.pe = PositionalEncoder(d_model)
-            self.tanh = nn.Tanh()
-        else:
-            print("INFO: Using ViT positional embedding")
-            self.pe = PositionalEmbedding(num_patches, d_model)
+        print("INFO: Using default positional encoder")
+        self.pe = PositionalEncoder(d_model)
+        self.tanh = nn.Tanh()
         encoder_layer = nn.TransformerEncoderLayer(d_model, nhead)
         self.transformer = nn.TransformerEncoder(encoder_layer, num_layers)
         self.lin_out = nn.Linear(d_model, num_joints_out*3)
@@ -41,40 +33,28 @@ class TransformerEncoder(nn.Module):
 
     def forward(self, x):
         bs = x.size(0)
-        if self.lift:
-            x = x.flatten(1).unsqueeze(1) #(bs,1,34)
-            x = self.pe(x)
-            x = self.transformer(x)
-            x = self.lin_out(x).squeeze(1)
-            x = self.tanh(x)
+        x = x.flatten(1).unsqueeze(1) #(bs,1,34)
+        x = self.pe(x)
+        x = self.transformer(x)
+        x = self.lin_out(x).squeeze(1)
+        x = self.tanh(x)
 
-        else:
-            x = self.pe(x)   #(bs,196,768)
-            x = self.transformer(x)
-            x = self.lin_out(x[:,0])
-
-        return x.reshape(bs, self.num_joints_out, 3)
+        return x.view(bs, self.num_joints_out, 3)
 
 
 class PETR(nn.Module):
     """
     PETR - Pose Estimation using TRansformer
     """
-    def __init__(self, device, lift=True):
+    def __init__(self, device):
         super().__init__()
         
-        self.lift = lift
         self.device = device
-        if self.lift:
-            self.backbone = HRNet(32, 17, 0.1)
-            pretrained_weight = "../weights/pose_hrnet_w32_256x192.pth"
-            self.backbone.load_state_dict(torch.load(pretrained_weight))
-            print("INFO: Pre-trained weights of HRNet loaded from {}".format(pretrained_weight))
-            self.transformer = TransformerEncoder(num_layers=8)
-        else:
-            self.patch_embed = PatchEmbedding()
-            self.transformer = TransformerEncoder(d_model=768, nhead=12, num_layers=12, lift=self.lift)
-            self.joint_token = nn.Parameter(torch.zeros(1,1,768))
+        self.backbone = HRNet(32, 17, 0.1)
+        pretrained_weight = "../weights/pose_hrnet_w32_256x192.pth"
+        self.backbone.load_state_dict(torch.load(pretrained_weight))
+        print("INFO: Pre-trained weights of HRNet loaded from {}".format(pretrained_weight))
+        self.transformer = TransformerEncoder(num_layers=8)
                                     
 
     def _decode_joints(self, heatmap):
@@ -101,18 +81,11 @@ class PETR(nn.Module):
 
 
     def forward(self, x):
-        if self.lift:
-            x = self.backbone(x)
-            x = self._decode_joints(x)
-            out_x = self.transformer(x.float())
-        else:
-            bs = x.shape[0]
-            x = self.patch_embed(x)[0]
-            joint_token = self.joint_token.repeat(bs,1,1)
-            emb = torch.cat([joint_token, x], dim=1)
-            out_x = self.transformer(emb)
+        x = self.backbone(x)
+        x = self._decode_joints(x)
+        out_x = self.transformer(x.float())
 
-        return x, out_x
+        return out_x
 
 
 if __name__ == "__main__":
@@ -124,12 +97,12 @@ if __name__ == "__main__":
         transforms.ToTensor(),  
         transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5]),
     ]) 
-    model = PETR(device="cuda:0", lift=True)
+    model = PETR(device="cuda:0")
     model = model.cuda()
     img = Image.open("dataset/S1/Seq1/imageSequence/video_8/frame006192.jpg")
     img = transforms(img)
     img = img.unsqueeze(0)
     print(img.shape)
     img = img.cuda()
-    _, output = model(img)
+    output = model(img)
     print(output.shape)
