282ddf6dcb7dcc7b7561b0462e13bdc57a0e766a
wuyenlin <finne0831@gmail.com>
2021-05-27 09:18:57 +0800

    mv func to File

--- common/dataloader.py
+++ common/dataloader.py
@@ -4,55 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
 from PIL import Image
-try:
-    from common.human import *
-    from common.peltra import *
-except ModuleNotFoundError:
-    from human import *
-    from peltra import *
 
 
 def collate_fn(batch):
     batch = list(filter(lambda x: x is not None, batch))
     return torch.utils.data.dataloader.default_collate(batch)
-
-
-def get_rot_from_vecs(vec1: np.array, vec2: np.array) -> np.array:
-    """ 
-    Find the rotation matrix that aligns vec1 to vec2
-    :param vec1: A 3d "source" vector
-    :param vec2: A 3d "destination" vector
-
-    :return R: A transform matrix (3x3) which when applied to vec1, aligns it with vec2.
-    
-    Such that vec2 = R @ vec1
-
-    (Credit to Peter from https://stackoverflow.com/questions/45142959/calculate-rotation-matrix-to-align-two-vectors-in-3d-space)
-    """
-    a, b = (vec1 / np.linalg.norm(vec1)).reshape(3), (vec2 / np.linalg.norm(vec2)).reshape(3)
-    v = np.cross(a, b)
-    c = np.dot(a, b)
-    s = np.linalg.norm(v)
-    kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
-    R = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
-    return R
-    
-
-def convert_gt(gt_3d: np.array, t_info) -> np.array:
-    """
-    Compare GT3D kpts with T pose and obtain 16 rotation matrices
-
-    :return R_stack: a (16,9) array with flattened rotation matrices for 16 bones
-    """
-    # process GT
-    bone_info = vectorize(gt_3d)[:,:3] # (16,3) bone vecs
-
-    num_bones = bone_info.shape[0]
-    R_stack = np.zeros([num_bones, 9])
-    # get rotation matrix for each bone
-    for k in range(num_bones):
-        R_stack[k,:] = get_rot_from_vecs(t_info[k,:], bone_info[k,:]).flatten()
-    return R_stack
 
 
 class Data:
@@ -70,23 +26,20 @@ class Data:
         else:
             vid_list = np.arange(6,8)
 
-        # initialize T pose as reference
-        h = Human(1.8, "cpu")
-        model = h.update_pose()
-        t_info = vectorize(model)[:,:3]
-
         for vid in vid_list:
             for frame in data[vid].keys():
                 bbox_start = data[vid][frame]['bbox_start']
-                pts_2d = (data[vid][frame]['2d_keypoints']).reshape(-1,2)
+                pts_2d = (data[vid][frame]["pts_2d"])
                 gt_2d = self.pop_joints(pts_2d) - bbox_start
 
-                pts_3d = (data[vid][frame]['3d_keypoints']).reshape(-1,3)
-                cam_3d = self.to_camera_coordinate(pts_2d, pts_3d, vid)
+                pts_3d = (data[vid][frame]["pts_3d"])
+                cam_3d = (data[vid][frame]["cam_3d"])
                 gt_3d = self.zero_center(cam_3d)/1000
 
+                vec_3d = (data[vid][frame]["vec_3d"])
+
                 self.gt_pts2d.append(gt_2d)
-                self.gt_vecs3d.append((convert_gt(gt_3d, t_info)))
+                self.gt_vecs3d.append(vec_3d)
                 self.img_path.append(data[vid][frame]['directory'])
 
     def __getitem__(self, index):
@@ -121,36 +74,6 @@ class Data:
         return new_skel
 
 
-    def get_intrinsic(self, camera):
-        """
-        Parse camera matrix from calibration file
-        :param camera: camera number (used in MPI dataset)
-        :return intrinsic matrix:
-        """
-        calib = open("./dataset/S1/Seq1/camera.calibration","r")
-        content = calib.readlines()
-        content = [line.strip() for line in content]
-        # 3x3 intrinsic matrix
-        intrinsic = np.array(content[7*camera+5].split(" ")[3:], dtype=np.float32)
-        intrinsic = np.reshape(intrinsic, (4,-1))
-        self.intrinsic = intrinsic[:3, :3]
-
-
-    def to_camera_coordinate(self, pts_2d, pts_3d, camera) -> np.array:
-        self.get_intrinsic(camera)
-        ret, R, t= cv.solvePnP(pts_3d, pts_2d, self.intrinsic, np.zeros(4), flags=cv.SOLVEPNP_EPNP)
-
-        # get extrinsic matrix
-        assert ret
-        R = cv.Rodrigues(R)[0]
-        E = np.concatenate((R,t), axis=1)  # [R|t], a 3x4 matrix
-    
-        pts_3d = cv.convertPointsToHomogeneous(self.pop_joints(pts_3d)).transpose().squeeze(1)
-        cam_coor = E @ pts_3d
-        cam_3d = cam_coor.transpose()
-        return cam_3d
-
-    
     def zero_center(self, cam) -> np.array:
         """
         translate root joint to origin (0,0,0)
