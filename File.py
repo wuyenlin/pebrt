260761837c262eadb0c330108a58075971ef4394
wuyenlin <finne0831@gmail.com>
2021-05-27 09:42:52 +0800

    fetched files from Gram

---
+++ File.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python3
+
+import os, sys
+import numpy as np 
+from tqdm import tqdm
+from common.Video import *
+from common.human import *
+
+
+
+class All(Video):
+    def __init__(self, S, Se, vid):
+        super().__init__(S, Se, vid)
+
+
+    def __del__(self):
+        print("Killed")
+
+
+    def bound_number(self, x, y, frame_size):
+    # make sure coordinates do not go beyond the frame
+        if x < 0 :
+            x = 0
+            if y < 0:
+                y = 0
+        elif y < 0 :
+            y = 0
+        elif x > frame_size.shape[0]:
+            x = frame_size.shape[0]
+            if y > frame_size.shape[1]:
+                y = frame_size.shape[1]
+        elif y > frame_size.shape[1]:
+            y = frame_size.shape[1]
+        return x, y
+
+
+    def in_box(self, pt, start, end):
+        return True if (start[0]<=pt[0]<=end[0] and start[1]<=pt[1]<=end[1]) else False
+
+
+    def check_valid(self, nframe, start, end):
+        """
+        Verify that all 3 keypoints are within the box
+        """
+        self.calib(nframe)
+        self.get_joints(nframe)
+        root = (self.proj_xS[4], self.proj_yS[4])
+        L_shoulder = (self.proj_xS[9], self.proj_yS[9])
+        R_shoulder = (self.proj_xS[14], self.proj_yS[14])
+        if ((self.in_box(root,start,end)) and (self.in_box(L_shoulder,start,end)) and (self.in_box(R_shoulder,start,end))):
+            return True
+        return False
+
+
+    def save_cropped(self, save_img=False, save_txt=True, full=False):
+        """
+        :param full: save original frame (in dimension of 2048x2048) if True
+        """
+        data = {}
+        cap = cv.VideoCapture(self.avi_path)
+        if (cap.isOpened()==False):
+            print("Error opening the video file.")
+
+        try:
+            if full:
+                os.mkdir("dataset/S{}/Seq{}/imageSequence/full_video_{}".format(self.S, self.Se, self.vid))
+            else:
+                os.mkdir("dataset/S{}/Seq{}/imageSequence/video_{}".format(self.S, self.Se, self.vid))
+        except FileExistsError:
+            pass
+
+        while (cap.isOpened()):
+            print("processing S{} Seq{} video_{}".format(self.S, self.Se, self.vid))
+            for k in tqdm(range(int(cap.get(cv.CAP_PROP_FRAME_COUNT)))):
+                ret, frame = cap.read()
+                if ret:
+                    x1, y1, x2, y2 = self.draw_bbox(k)
+                    x1, y1 = self.bound_number(x1, y1, frame)
+                    x2, y2 = self.bound_number(x2, y2, frame)
+                    start = (x1, y1)
+                    end = (x2, y2)
+                    if not self.check_valid(k, start, end): 
+                        continue
+
+
+                    if full:
+                        filename = os.path.join("dataset", "S{}/Seq{}/imageSequence/full_video_{}/frame{:06}.jpg".format(self.S, self.Se, self.vid, k))
+                    else:
+                        filename = os.path.join("dataset", "S{}/Seq{}/imageSequence/video_{}/frame{:06}.jpg".format(self.S, self.Se, self.vid, k))
+
+                    if end[0]-start[0]==end[1]-start[1]:
+                        data[k] = {}
+                        if save_img:
+                            try:
+                                cropped_frame = frame[start[1]:end[1], start[0]:end[0]]
+                                if full:
+                                    cv.imwrite(filename, cv.resize(frame, (512,512), interpolation=cv.INTER_AREA))
+                                else:
+                                    cv.imwrite(filename, cropped_frame)
+                            except FileExistsError:
+                                pass
+
+                        if save_txt:
+                            h = Human(1.8, "cpu")
+                            model = h.update_pose()
+                            t_info = vectorize(model)[:,:3]
+                            pts_2d, pts_3d = self.imgPoint.reshape(-1,2), self.objPoint.reshape(-1,3)
+                            cam_3d = self.to_camera_coordinate(pts_2d, pts_3d)
+
+                            data[k]["directory"] = filename
+                            data[k]["bbox_start"] = start
+                            data[k]["pts_2d"] = pts_2d
+                            data[k]["pts_3d"] = pts_3d
+                            data[k]["cam_3d"] = cam_3d
+                            data[k]["vec_3d"] = convert_gt(cam_3d, t_info)
+            break
+        if full:
+            np.savez_compressed("dataset/S{}/Seq{}/imageSequence/full_video_{}".format(self.S,self.Se,self.vid), data)
+        else:
+            np.savez_compressed("dataset/S{}/Seq{}/imageSequence/video_{}".format(self.S,self.Se,self.vid), data)
+
+        cap.release()
+
+
+def save_frame(human):
+    for seq in (1,2):
+        for vid in [0,1,2,4,5,6,7,8]:
+            v = All(human, seq, vid)
+            v.save_cropped(False, True, False)
+
+
+def merge_npz(human):
+    merge_data = []
+    for s in [1,2]:
+        for k in [0,1,2,4,5,6,7,8]:
+            npz = "./dataset/S{}/Seq{}/imageSequence/video_{}.npz".format(human,s,k)
+            t = np.load(npz, allow_pickle=True)
+            t = t['arr_0'].reshape(1,-1)
+            merge_data.append(*t)
+        np.savez_compressed("./dataset/S{}/Seq1/imageSequence/S{}".format(human,human), merge_data)
+    print("saved")
+
+
+if __name__ == "__main__": 
+    for human in range(1,9):
+        save_frame(human)
+        merge_npz(human)
---
+++ common/human.py
@@ -0,0 +1,250 @@
+import numpy as np
+import cv2 as cv
+import cmath
+from math import sin, cos
+import matplotlib.pyplot as plt
+import torch.nn.functional as f
+import torch
+
+
+def rot(euler) -> torch.tensor:
+    """
+    General rotation matrix
+    :param a: yaw (rad)
+    :param b: pitch (rad)
+    :param r: roll (rad)
+    
+    :return R: a rotation matrix R
+    """
+    a, b, r = euler[0], euler[1], euler[2]
+    row1 = torch.tensor([cos(a)*cos(b), cos(a)*sin(b)*sin(r)-sin(a)*cos(r), cos(a)*sin(b)*cos(r)+sin(a)*sin(r)])
+    row2 = torch.tensor([sin(a)*cos(b), sin(a)*sin(b)*sin(r)+cos(a)*cos(r), sin(a)*sin(b)*cos(r)-cos(a)*sin(r)])
+    row3 = torch.tensor([-sin(b), cos(b)*sin(r), cos(b)*cos(r)])
+    R = torch.stack((row1, row2, row3), 0)
+    assert cmath.isclose(torch.det(R), 1, rel_tol=1e-04), torch.det(R)
+    return R.flatten()
+
+
+def euler_from_rot(R: np.array) -> np.array:
+    angles = cv.RQDecomp3x3(R)[0]
+    return np.radians(angles)
+
+
+class Human:
+    """
+    Implementation of Winter human model
+    """
+    def __init__(self, H, device="cuda:0"):
+        self.device = device
+        self.half_face = 0.066*H
+        self.neck = 0.052*H
+        self.upper_spine, self.lower_spine = 0.144*H, 0.144*H
+        self.clavicle = 0.129*H
+        self.upper_arm, self.lower_arm = 0.186*H, 0.146*H
+
+        self.pelvis = 0.191*H
+        self.thigh, self.calf = 0.245*H, 0.246*H
+        self.root = torch.zeros(3)
+
+# TODO: yeah something is wrong
+        self.constraints = {
+            'lower_spine': ((-1.0,1.0), (0,0), (-1.0,1.0)),
+            'upper_spine': ((-2.0,2.0), (0,0), (-2.0,2.0)),
+            'neck': ((-1.0,1.0), (0,0), (0,0)),
+            'head': ((-1.0,1.0), (0,0), (-1.0,1.57)),
+
+            'l_clavicle': ((0,0), (0,0), (0,0)),
+            'l_upper_arm': ((-3.14,3.14), (-0.1,1.7), (0,0)),
+            'l_lower_arm': ((-6.14,3.14), (0,0), (0,0)),
+            'r_clavicle': ((0,0), (0,0), (0,0)),
+            'r_upper_arm': ((-3.14,3.14), (-1.7,0.1), (0,0)),
+            'r_lower_arm': ((-3.14,6.14), (0,0), (0,0)),
+
+            'l_hip': ((0,0), (0,0), (0,0)),
+            'l_thigh': ((-1.57,1.0), (0,0), (-1.57,1.57)),
+            'l_calf': ((-1.57,1.0), (0,0), (-4.71,1.57)),
+            'r_hip': ((0,0), (0,0), (0,0)),
+            'r_thigh': ((-1.0,1.57), (0,0), (-1.57,1.57)),
+            'r_calf': ((-1.0,1.57), (0,0), (-4.71,1.57)),
+        }
+
+
+    def _init_bones(self):
+        """get bones as vectors"""
+        self.bones = {
+            'lower_spine': torch.tensor([0, -self.lower_spine, 0]),
+            'upper_spine': torch.tensor([0, -self.upper_spine, 0]),
+            'neck': torch.tensor([0, -self.neck, 0]),
+            'head': torch.tensor([0, -self.half_face, 0]),
+
+            'l_clavicle': torch.tensor([self.clavicle, 0, 0]),
+            'l_upper_arm': torch.tensor([self.upper_arm, 0, 0]),
+            'l_lower_arm': torch.tensor([self.lower_arm, 0, 0]),
+            'r_clavicle': torch.tensor([-self.clavicle, 0, 0]),
+            'r_upper_arm': torch.tensor([-self.upper_arm, 0, 0]),
+            'r_lower_arm': torch.tensor([-self.lower_arm, 0, 0]),
+
+            'l_hip': torch.tensor([self.pelvis/2, 0, 0]),
+            'l_thigh': torch.tensor([0, self.thigh, 0]),
+            'l_calf': torch.tensor([0, self.calf, 0]),
+            'r_hip': torch.tensor([-self.pelvis/2, 0, 0]),
+            'r_thigh': torch.tensor([0, self.thigh, 0]),
+            'r_calf': torch.tensor([0, self.calf, 0])
+        }
+        for bone in self.bones.keys():
+            self.bones[bone] = self.bones[bone].to(self.device)
+        
+
+    def check_constraints(self, bone, R: np.array):
+        """
+        This function punishes if NN outputs are beyond joint rotation constraints.
+        """
+        punish_w = 1
+        euler_angles = euler_from_rot(np.array(R).reshape(3,-1))
+        for i in range(3):
+            low = self.constraints[bone][i][0]
+            high = self.constraints[bone][i][1]
+            if euler_angles[i] < low:
+                euler_angles[i] = low
+                punish_w += 0.5
+            elif euler_angles[i] > high:
+                euler_angles[i] = high
+                punish_w += 0.5
+        return rot(euler_angles), punish_w
+
+
+    def sort_rot(self, elem):
+        """
+        :param ang: a list of 144 elements (9 * 16)
+        process PETRA output to rotation matrix of 16 bones
+        """
+        elem = elem.flatten()
+        self.rot_mat, self.punish_list = {}, []
+        k = 0
+        for bone in self.constraints.keys():
+            R = elem[9*k:9*(k+1)]
+            # R, punish_w = self.check_constraints(bone, R)
+            # self.punish_list.append(punish_w)
+
+            R = f.normalize(R.to(torch.float32).view(3,-1))
+            self.rot_mat[bone] = R
+            k += 1
+
+
+    def update_bones(self, elem=None):
+        """
+        Initiates a T-Pose human model and rotate each bone using the given rotation matrices
+        :return model: a numpy array of (17,3)
+        """
+        self._init_bones()
+        if elem is not None:
+            self.sort_rot(elem)
+            self.bones = {bone: self.rot_mat[bone]@self.bones[bone] for bone in self.constraints.keys()}
+
+
+    def update_pose(self, elem=None, debug=False):
+        """
+        Assemble bones to make a human body
+        """
+        self.update_bones(elem)
+        if debug:
+            for bone in self.constraints.keys():
+                print(bone, ":\n", self.rot_mat[bone])
+
+        root = self.root.to(self.device)
+        lower_spine = self.bones['lower_spine']
+        neck = self.bones['upper_spine'] + lower_spine
+        chin = self.bones['neck'] + neck
+        nose = self.bones['head'] + chin
+
+        l_shoulder = self.bones['l_clavicle'] + neck
+        l_elbow = self.bones['l_upper_arm'] + l_shoulder
+        l_wrist = self.bones['l_lower_arm'] + l_elbow
+        r_shoulder = self.bones['r_clavicle'] + neck
+        r_elbow = self.bones['r_upper_arm'] + r_shoulder
+        r_wrist = self.bones['r_lower_arm'] + r_elbow
+
+        l_hip = self.bones['l_hip']
+        l_knee = self.bones['l_thigh'] + l_hip
+        l_ankle = self.bones['l_calf'] + l_knee
+        r_hip = self.bones['r_hip']
+        r_knee = self.bones['r_thigh'] + r_hip
+        r_ankle = self.bones['r_calf'] + r_knee
+
+        self.model = torch.stack((neck, lower_spine, root, chin, nose,
+                l_shoulder, l_elbow, l_wrist, r_shoulder, r_elbow, r_wrist,
+                l_hip, l_knee, l_ankle, r_hip, r_knee, r_ankle), 0)
+        
+        return self.model
+
+
+def vectorize(gt_3d) -> torch.tensor:
+    """
+    process gt_3d (17,3) into a (16,4) that contains bone vector and length
+    :return bone_info: [unit bone vector (,3) + bone length (,1)]
+    """
+    indices = (
+        (2,1), (1,0), (0,3), (3,4),  # spine + head
+        (0,5), (5,6), (6,7), 
+        (0,8), (8,9), (9,10), # arms
+        (2,14), (11,12), (12,13),
+        (2,11), (14,15), (15,16), # legs
+    )
+    num_bones = len(indices)
+    try:
+        gt_3d_tensor = torch.from_numpy(gt_3d)
+    except TypeError:
+        gt_3d_tensor = gt_3d
+    bone_info = torch.zeros([num_bones, 4], requires_grad=False) # (16, 4)
+    for i in range(num_bones):
+        vec = gt_3d_tensor[indices[i][1],:] - gt_3d_tensor[indices[i][0],:]
+        vec_len = torch.linalg.norm(vec)
+        unit_vec = vec/vec_len
+        bone_info[i,:3], bone_info[i,3] = unit_vec, vec_len
+    return bone_info
+
+
+def vis_model(model):
+    indices = (
+        (0,1), (0,3), (1,2), (3,4),  # spine + head
+        (0,5), (0,8), # clavicle
+        (5,6), (6,7), (8,9), (9,10), # arms
+        (2,14), (2,11), # pelvis
+        (11,12), (12,13), (14,15), (15,16), # legs
+    )
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    for p in model:
+        ax.scatter(p[0], p[1], p[2], c='r')
+
+    for index in indices:
+        xS = (model[index[0]][0], model[index[1]][0])
+        yS = (model[index[0]][1], model[index[1]][1])
+        zS = (model[index[0]][2], model[index[1]][2])
+        ax.plot(xS, yS, zS)
+    ax.view_init(elev=-80, azim=-90)
+    ax.autoscale()
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+    plt.show()
+
+
+def rand_pose():
+    h = Human(1.8, "cpu")
+    euler = (0,0,0)
+    a = rot(euler).repeat(16)
+    model = h.update_pose(a)
+    print(model)
+    print(h.punish_list)
+    vis_model(model)
+
+
+if __name__ == "__main__":
+
+
+    import timeit
+    start = timeit.default_timer()
+    rand_pose()
+    stop = timeit.default_timer()
+    print(stop-start)
