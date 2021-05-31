34f3f64b94c55cc4ab617a61919d98364be808e8
wuyenlin <finne0831@gmail.com>
2021-05-31 14:48:52 +0800

    ready to run

--- this.sh
+++ this.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CMD="python3 angle.py --lift --resume ./angle_checkpoint/epoch_5.bin"
+CMD="python3 lift.py"
 #CMD="python3 angle.py --lift --resume ./angle_checkpoint/trans.bin"
 echo $CMD
 $CMD
