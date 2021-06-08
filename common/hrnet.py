3c8dbf4719efdba6c4f903a99de7b1a8810493d0
wuyenlin <finne0831@gmail.com>
2021-06-08 10:57:51 +0800

    merge misc here

--- common/hrnet.py
+++ common/hrnet.py
@@ -3,14 +3,35 @@ import torch
 from torch import nn
 from torch import nn, Tensor
 
-from collections import OrderedDict
-import copy
-from typing import Optional, List
 
-try:
-    from common.misc import *
-except ModuleNotFoundError:
-    from misc import *
+def coco_mpi(coco_joints):
+    """
+    convert predicted COCO joints to MPI-INF-3DHP joints order
+    """
+    mpi_joints = np.zeros_like(coco_joints)
+    mpi_joints[4,:] = coco_joints[0,:]
+    # arms
+    mpi_joints[5,:] = coco_joints[6,:]
+    mpi_joints[6,:] = coco_joints[8,:]
+    mpi_joints[7,:] = coco_joints[10,:]
+    mpi_joints[8,:] = coco_joints[5,:]
+    mpi_joints[9,:] = coco_joints[7,:]
+    mpi_joints[10,:] = coco_joints[9,:]
+    # legs
+    mpi_joints[11,:] = coco_joints[12,:]
+    mpi_joints[12,:] = coco_joints[14,:]
+    mpi_joints[13,:] = coco_joints[16,:]
+    mpi_joints[14,:] = coco_joints[11,:]
+    mpi_joints[15,:] = coco_joints[13,:]
+    mpi_joints[16,:] = coco_joints[15,:]
+    # spine (manual interpolation)
+    mpi_joints[0,:] = (coco_joints[5,:] + coco_joints[6,:])/2
+    mpi_joints[2,:] = (coco_joints[11,:] + coco_joints[12,:])/2
+    mpi_joints[1,:] = (mpi_joints[0,:] + mpi_joints[2,:])/2
+    mpi_joints[3,:] = (mpi_joints[0,:] + mpi_joints[4,:])/2
+
+    return mpi_joints
+
 
 
 class Bottleneck(nn.Module):
