dec2ba418f4f9f495b2f46d570034d42890d1655
wuyenlin <finne0831@gmail.com>
2021-06-17 15:17:01 +0800

    trimmed long lines to 2 lines

--- common/dataloader.py
+++ common/dataloader.py
@@ -40,7 +40,7 @@ def convert_gt(gt_3d: np.array, t_info, dataset="mpi") -> np.array:
     :return R_stack: a (16,9) arrays with flattened rotation matrix for 16 bones
     """
     # process GT
-    bone_info = vectorize(gt_3d, dataset=dataset)[:,:3] # (16,3) bone vecs
+    bone_info = vectorize(gt_3d, dataset)[:,:3] # (16,3) bone vecs
 
     num_row = bone_info.shape[0]
     R_stack = np.zeros([num_row, 9])
@@ -52,7 +52,7 @@ def convert_gt(gt_3d: np.array, t_info, dataset="mpi") -> np.array:
 
 
 class Data:
-    def __init__(self, npz_path, transforms = None, train=True):
+    def __init__(self, npz_path, transforms=None, train=True):
         self.img_path = []
         self.gt_pts2d = []
         self.gt_pts3d = []
@@ -79,12 +79,13 @@ class Data:
 
             for action in data3d_list:
                 for frame in range(data_3d[action].shape[0]):
-                    gt_2d = self.zero_center(data_2d[action][0][frame,:,:], dataset="h36m")
-                    gt_3d = self.zero_center(self.remove_joints(data_3d[action][frame,:,:], dataset="h36m"), dataset="h36m")
+                    gt_2d = self.zero_center(data_2d[action][0][frame,:,:], "h36m")
+                    gt_3d = self.zero_center(self.remove_joints( \
+                            data_3d[action][frame,:,:], "h36m"), "h36m")
 
                     self.gt_pts2d.append(gt_2d)
                     self.gt_pts3d.append(gt_3d)
-                    self.gt_vecs3d.append((convert_gt(gt_3d, t_info, dataset="h36m")))
+                    self.gt_vecs3d.append((convert_gt(gt_3d, t_info, "h36m")))
                     self.img_path.append(frame)
 
             
@@ -108,6 +109,7 @@ class Data:
                     self.gt_pts3d.append(gt_3d)
                     self.gt_vecs3d.append((convert_gt(gt_3d, t_info)))
                     self.img_path.append(data[vid][frame]["directory"])
+            print(len(self.gt_pts2d))
 
     def __getitem__(self, index):
         try:
@@ -121,16 +123,15 @@ class Data:
             return None
         # return img_path, img, kpts_2d, vecs_3d
         return frame, kpts_2d, kpts_3d, vecs_3d
+        
 
     def __len__(self):
-        return len(self.img_path)
+        return len(self.gt_pts2d)
     
 
     def remove_joints(self, kpts, dataset="mpi"):
         """
-        Get 17 joints from the original 28 (MPI) or 32 (Human3.6M)
-        :param kpts: orginal kpts from MPI-INF-3DHP (an array of (28,3))
-        :return new_skel: 
+        Get 17 joints from the original 28 (MPI) / 32 (Human3.6M)
         """
         new_skel = np.zeros([17,3]) if kpts.shape[-1]==3 else np.zeros([17,2])
         if dataset == "mpi":
@@ -161,20 +162,19 @@ class Data:
         content = [line.strip() for line in content]
         # 3x3 intrinsic matrix
         intrinsic = np.array(content[7*camera+5].split(" ")[3:], dtype=np.float32)
-        intrinsic = np.reshape(intrinsic, (4,-1))
-        self.intrinsic = intrinsic[:3, :3]
+        self.intrinsic = intrinsic.reshape(4,-1)[:3, :3]
 
 
     def to_camera_coordinate(self, pts_2d, pts_3d, camera) -> np.array:
         self.get_intrinsic(camera)
-        ret, R, t= cv.solvePnP(pts_3d, pts_2d, self.intrinsic, np.zeros(4), flags=cv.SOLVEPNP_EPNP)
+        ret, R, t = cv.solvePnP(pts_3d, pts_2d, self.intrinsic, np.zeros(4), flags=cv.SOLVEPNP_EPNP)
 
         # get extrinsic matrix
         assert ret
         R = cv.Rodrigues(R)[0]
         E = np.concatenate((R,t), axis=1)  # [R|t], a 3x4 matrix
     
-        pts_3d = cv.convertPointsToHomogeneous(self.pop_joints(pts_3d)).transpose().squeeze(1)
+        pts_3d = cv.convertPointsToHomogeneous(self.remove_joints(pts_3d)).transpose().squeeze(1)
         cam_coor = E @ pts_3d
         cam_3d = cam_coor.transpose()
         return cam_3d
@@ -194,6 +194,7 @@ def try_load():
     from torchvision import transforms
     from torch.utils.data import DataLoader
     train_npz = "./data_h36m.npz"
+    train_npz = "./dataset/S1/Seq1/imageSequence/S1.npz"
     train_dataset = Data(train_npz, transforms, True)
     trainloader = DataLoader(train_dataset, batch_size=4, 
                         shuffle=True, num_workers=8, drop_last=True)
