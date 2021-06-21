10da918de961ded931a2504e28aafdf75f4ee2bc
wuyenlin <finne0831@gmail.com>
2021-06-21 11:38:43 +0800

    added h36m and mpi dataset processing files

---
+++ common/h36m_dataset.py
@@ -0,0 +1,196 @@
+import os
+import numpy as np
+import cv2 as cv
+from numpy.lib.npyio import save
+from tqdm import tqdm
+
+
+
+def merge_2d3d():
+    """
+    This function is for merging the 2D and 3D .npz file made by 
+    data/prepare_data_h36m.py in VideoPose3D.
+    Can be used for PEBRT.
+    """
+    merge_data = []
+    files = ["./data_2d_h36m_gt.npz","./data_3d_h36m.npz"]
+    print("Processing...")
+    for item in files:
+        t = np.load(item, allow_pickle=True)
+        t = t["positions_2d"].reshape(1,-1) if item.endswith("2d_h36m_gt.npz") else t["positions_3d"].reshape(1,-1)
+        merge_data.append(*t)
+    filename = "./h36m/data_h36m"
+    np.savez_compressed(filename, positions_2d=merge_data[0], positions_3d=merge_data[1])
+    print("saved {}.npz".format(filename))
+
+
+class Video:
+    def __init__(self, S, action, cam):
+        self.S = S
+        self.action = action
+        if S == "S1":
+            action = action.replace("Photo", "TakingPhoto") \
+                            .replace("WalkDog", "WalkingDog")
+        self.vid_path = "./h36m/{}/Videos/{}.{}.mp4".format(S, action, cam)
+        self.cam = 54138969
+
+        self.img_path = "./h36m/{}/{}.{}/".format(S, self.action, cam)
+        self.npz_name = "./h36m/{}/{}_{}".format(S, S, self.action)
+
+        data_2d = np.load("./h36m/data_2d_h36m_gt.npz", allow_pickle=True)
+        self.annot2D = data_2d["positions_2d"].reshape(1,-1)[0][0][S][self.action][0]
+        data_3d = np.load("./h36m/data_3d_h36m.npz", allow_pickle=True)
+        self.annot3D = data_3d["positions_3d"].reshape(1,-1)[0][0][S][self.action]
+
+    
+    def __del__(self):
+        print("Killed")
+    
+
+    def draw_bbox(self, nframe):
+        xS, yS = self.annot2D[nframe,:,0], self.annot2D[nframe,:,1]
+
+        thresh = 100
+        x1, y1 = int(min(xS)-thresh) , int(min(yS)-thresh)
+        x2, y2 = int(max(xS)+thresh) , int(max(yS)+thresh) 
+
+        w, h = x2-x1, y2-y1
+        max_wh = np.max([w,h])
+        hp = int((max_wh - w) / 2)
+        vp = int((max_wh - h) / 2)
+        x1, y1 = x1-hp, y1-vp
+        x2, y2 = x2+hp, y2+vp
+        
+        return x1, y1, x2, y2
+
+
+    def bound_number(self, x, y, frame_size):
+        """make sure coordinates do not go beyond the frame"""
+        if x < 0 :
+            x = 0
+            if y < 0:
+                y = 0
+        elif y < 0 :
+            y = 0
+        elif x > frame_size.shape[0]:
+            x = frame_size.shape[0]
+            if y > frame_size.shape[1]:
+                y = frame_size.shape[1]
+        elif y > frame_size.shape[1]:
+            y = frame_size.shape[1]
+        return x, y
+
+
+    def save(self, save_img=False, save_npz=True):
+        data = {}
+        cap = cv.VideoCapture(self.vid_path)
+        if (cap.isOpened() == False):
+            print("Error opening the video file: " + self.vid_path)
+
+        # if os.path.exists(self.npz_name + ".npz"):
+        #     print("The dataset already exists at", self.npz_name + ".npz")
+        try:
+            os.mkdir("./h36m/{}/{}.{}"\
+                        .format(self.S, self.action, self.cam))
+        except FileExistsError:
+            pass
+
+        while (cap.isOpened()):
+            print("processing {}, {}...".format(self.S, self.action))
+            for k in tqdm(range(int(cap.get(cv.CAP_PROP_FRAME_COUNT))-1)):
+                ret, frame = cap.read()
+                assert ret
+                if cv.waitKey(25) & 0xFF == ord("q"):
+                    break
+
+                try:
+                    x1, y1, x2, y2 = self.draw_bbox(k)
+                except IndexError:
+                    continue
+                x1, y1 = self.bound_number(x1, y1, frame)
+                x2, y2 = self.bound_number(x2, y2, frame)
+                start = (x1, y1)
+                end = (x2, y2)
+
+                filename = "./h36m/{}/{}.{}/frame{:06}.jpg"\
+                            .format(self.S, self.action, self.cam, k)
+
+                if end[0]-start[0] == end[1]-start[1]:
+                    data[k] = {}
+                    if save_img:
+                        cropped_frame = frame[start[1]:end[1], start[0]:end[0]]
+                        cv.imwrite(filename, cropped_frame)
+
+                    if save_npz:
+                        data[k]["directory"] = filename
+                        data[k]["bbox_start"] = start
+                        data[k]["bbox_end"] = end
+                        data[k]["positions_2d"] = self.annot2D.reshape(-1,2)
+                        data[k]["positions_3d"] = self.annot3D.reshape(-1,3)
+            cap.release()
+            return data if save_npz else None
+
+
+def main(subject_action):
+    """
+    This function is for merging the 2D and 3D .npz file made by 
+    data/prepare_data_h36m.py in VideoPose3D.
+    Can be used for PEBRT.
+    """
+    tabs = {}
+    for s in subject_action.keys():
+        for action in subject_action[s]:
+            v = Video(s, action, 54138969)
+            data = (v.save(save_img=False, save_npz=True))
+            cat = s + "/" + action
+            tabs[cat] = data
+    print("Merging all npz files.")
+    filename = "./h36m/data_h36m_frame"
+    np.savez_compressed(filename, **tabs)
+    print("saved {}.npz".format(filename))
+    print("Done!")
+
+
+if __name__ == "__main__":
+    subject_action = {
+        "S1": ["Photo", "Phoning", "Sitting 1", "Purchases", "Purchases 1", "WalkTogether", "Sitting 2", "WalkDog", 
+                "Smoking 1", "Phoning 1", "Walking 1", "Walking", "Discussion 1", "SittingDown", "Directions", 
+                "Greeting 1", "Eating 2", "Eating", "Photo 1", "WalkTogether 1", 
+                "Greeting", "Directions 1", "WalkDog 1", "Posing 1", "Waiting", "Posing", 
+                "Discussion", "Smoking", "Waiting 1", "SittingDown 2"],
+
+        "S5": ["Phoning", "Sitting 1", "SittingDown 1", "Purchases", "Purchases 1", 
+                "WalkTogether", "Discussion 3", "Sitting", "Waiting 2", "Smoking 1", 
+                "Photo 2", "Phoning 1", "Walking 1", "Eating 1", "WalkDog 1", "Walking", 
+                "SittingDown", "Greeting 1", "Eating", "WalkTogether 1", "Greeting 2", 
+                "Directions 2", "Directions 1", "Posing 1", "Discussion 2", 
+                "Photo", "WalkDog", "Posing", "Smoking", "Waiting 1"],
+
+        "S6": ["Phoning", "Sitting 1", "SittingDown 1", "Purchases", "Purchases 1", 
+                "WalkTogether", "Sitting 2", "Smoking 1", "Waiting 3", "Phoning 1", 
+                "Photo 1", "Walking 1", "Eating 1", "WalkDog 1", "Walking", "Discussion 1", 
+                "SittingDown", "Directions", "Greeting 1", "Eating 2", "WalkTogether 1", "Greeting", 
+                "Directions 1", "Posing 2", "Waiting", "Photo", "WalkDog", "Posing", "Discussion", "Smoking"],
+
+        "S7": ["Phoning", "Phoning 2", "Sitting 1", "SittingDown 1", "Purchases", "Purchases 1", "WalkTogether", 
+                "Sitting", "Waiting 2", "Smoking 1", "Photo 1", "Walking 1", "Eating 1", "WalkDog 1", 
+                "Discussion 1", "SittingDown", "Directions", "Greeting 1", "Eating", "WalkTogether 1", 
+                "Greeting", "Directions 1", "Posing 1", "Photo", "WalkDog", "Posing", 
+                "Discussion", "Smoking", "Waiting 1", "Walking 2"],
+
+        "S8": ["Phoning", "Sitting 1", "SittingDown 1", "Purchases", "Purchases 1", "Sitting", "Smoking 1", 
+                "Phoning 1", "Photo 1", "Walking 1", "Eating 1", "WalkDog 1", "Walking", "Discussion 1", "SittingDown", 
+                "Directions", "Greeting 1", "WalkTogether 2", "Eating", "WalkTogether 1", "Greeting", "Directions 1", 
+                "Posing 1", "Waiting", "Photo", "WalkDog", "Posing", "Discussion", "Smoking", "Waiting 1"],
+
+        "S9": ["Phoning", "Sitting 1", "SittingDown 1", "Purchases", "Purchases 1", "WalkTogether", "Sitting", "Smoking 1", 
+                "Phoning 1", "Photo 1", "Walking 1", "Eating 1", "WalkDog 1", "Walking", "Discussion 1", "SittingDown", 
+                "Directions", "Greeting 1", "Eating", "WalkTogether 1", "Greeting", "Directions 1", "Posing 1", "Discussion 2", 
+                "Waiting", "Photo", "WalkDog", "Posing", "Smoking", "Waiting 1"],
+
+        "S11": ["Phoning 2", "Sitting 1", "SittingDown 1", "Purchases", "Purchases 1", "WalkTogether", "Sitting", "Photo 1", 
+                "Walking 1", "Eating 1", "WalkDog 1", "Walking", "Discussion 1", "Phoning 3", "Smoking 2", "SittingDown", 
+                "Eating", "WalkTogether 1", "Greeting", "Greeting 2", "Posing 1", "Discussion 2", "Waiting", 
+                "Photo", "WalkDog", "Posing", "Smoking", "Waiting 1"],
+    }
+    main(subject_action)
--- common/mpi_dataset.py
+++ common/mpi_dataset.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
 import scipy.io as sio
-from numpy.linalg import norm
 import numpy as np
-import math
 import cv2 as cv
 import os
 from tqdm import tqdm
@@ -19,7 +16,6 @@ class Video:
         self.avi_path = "dataset/S{}/Seq{}/imageSequence/video_{}.avi".format(S,Se,vid)
         self.calib_path = "dataset/S{}/Seq{}/camera.calibration".format(S,Se)
 
-        self.camera = vid # get camera number
         self.annot3D = sio.loadmat(self.mat_path)['annot3']
         self.annot2D = sio.loadmat(self.mat_path)['annot2']
 
@@ -28,7 +24,7 @@ class Video:
         print("Killed")
     
     def draw_bbox(self, nframe):
-        coordinates = self.annot2D[self.camera][0][nframe]
+        coordinates = self.annot2D[self.vid][0][nframe]
         xS = []
         yS = []
         for k in range(0, len(coordinates)):
@@ -53,17 +49,10 @@ class Video:
     def get_cross(self, nframe):
         """
         Calculate the cross product given a frame
-
-        Output: a 1x3 numpy array
         """
-        objPoint = self.annot3D[self.camera][0][nframe]
-        objPoint = np.array(objPoint.reshape(-1,3), dtype=np.float32)
-        pt_list = [4, 9, 14]
-        p0 = np.array(objPoint[pt_list[0]])
-        p1 = np.array(objPoint[pt_list[1]])
-        p2 = np.array(objPoint[pt_list[2]])
-        n_vec = np.cross(p1-p0, p2-p0)
-        self.n_vec = n_vec
+        objPoint = self.annot3D[self.vid][0][nframe]
+        obj = np.array(objPoint.reshape(-1,3), dtype=np.float32)
+        self.n_vec = np.cross(obj[9]-obj[4], obj[14]-obj[4])
 
 
     def cam_matrix(self):
@@ -74,17 +63,15 @@ class Video:
         content = calib.readlines()
         content = [line.strip() for line in content]
         # 3x3 intrinsic matrix
-        camMatrix = np.array(content[7*self.camera+5].split(" ")[3:], dtype=np.float32)
-        camMatrix = np.reshape(camMatrix, (4,-1))
-        camMatrix = camMatrix[0:3,0:3]
-        self.camMatrix = camMatrix 
+        camMatrix = np.array(content[7*self.vid+5].split(" ")[3:], dtype=np.float32)
+        self.camMatrix = camMatrix.reshape(4,-1)[0:3,0:3]
     
 
     def parse_frame(self, nframe):
         self.get_cross(nframe)
-        self.objPoint = self.annot3D[self.camera][0][nframe]
+        self.objPoint = self.annot3D[self.vid][0][nframe]
         self.objPoint = np.array(self.objPoint.reshape(-1,3), dtype=np.float32)
-        self.imgPoint = self.annot2D[self.camera][0][nframe]
+        self.imgPoint = self.annot2D[self.vid][0][nframe]
         self.imgPoint = np.array(self.imgPoint.reshape(-1,2), dtype=np.float32)
         self.root = self.objPoint[4]
 
@@ -101,43 +88,19 @@ class Video:
         self.dist = np.zeros(4)
 
 
-    def get_arrow(self, nframe):
-        self.parse_frame(nframe)
-        arrow = []
-        arrow.append(self.root)
-        arrow.append(400*(self.n_vec/norm(self.n_vec))+self.root) # adjust normal vector length
-        projected, _ = cv.projectPoints(np.float32(arrow), \
-            self.rvec, self.tvec, self.camMatrix, self.dist)
-        arrow_root = (int(projected[0][0][0]), int(projected[0][0][1]))
-        arrow_end = (int(projected[1][0][0]), int(projected[1][0][1]))
-        self.arrow_root = arrow_root
-        self.arrow_end = arrow_end
-        self.angle = np.dot(self.n_vec, np.array([1,0,0]))/norm(self.n_vec)
-        self.angle = math.acos(self.angle)*180/(math.pi)
-    
-
-    def valid_arrow(self, pt):
-        """
-        A boolean function that verifies whether a given point falls within the frame
-        """
-        return True if (0 <= pt[0] <= 2048) and (0 <= pt[1] <= 2048) else False
-
-
     def get_joints(self, nframe):
         self.parse_frame(nframe)
-        projected, _ = cv.projectPoints(self.objPoint, self.rvec, self.tvec, self.camMatrix, self.dist)
+        projected, _ = cv.projectPoints(self.objPoint, self.rvec, self.tvec, \
+                                        self.camMatrix, self.dist)
         projected = projected.reshape(28,-1)
-        proj_xS = []
-        proj_yS = []
+        self.proj_xS, self.proj_yS = [], []
         for x,y in projected:
-            proj_xS.append(int(x))
-            proj_yS.append(int(y))
-        self.proj_xS = proj_xS
-        self.proj_yS = proj_yS
+            self.proj_xS.append(int(x))
+            self.proj_yS.append(int(y))
 
 
-    # make sure coordinates do not go beyond the frame
     def bound_number(self, x, y, frame_size):
+        """make sure coordinates do not go beyond the frame"""
         if x < 0 :
             x = 0
             if y < 0:
@@ -151,6 +114,7 @@ class Video:
         elif y > frame_size.shape[1]:
             y = frame_size.shape[1]
         return x, y
+
 
     def in_box(self, pt, start, end):
         return True if (start[0]<=pt[0]<=end[0] and start[1]<=pt[1]<=end[1]) else False
@@ -173,7 +137,7 @@ class Video:
     def save_cropped(self, save_img=False, save_npz=True, full=False):
         data = {}
         cap = cv.VideoCapture(self.avi_path)
-        if (cap.isOpened()==False):
+        if (cap.isOpened() == False):
             print("Error opening the video file.")
 
         try:
@@ -188,47 +152,45 @@ class Video:
             print("processing S{} Seq{} video_{}".format(self.S, self.Se, self.vid))
             for k in tqdm(range(int(cap.get(cv.CAP_PROP_FRAME_COUNT)))):
                 ret, frame = cap.read()
-                if ret:
-                    x1, y1, x2, y2 = self.draw_bbox(k)
-                    x1, y1 = self.bound_number(x1, y1, frame)
-                    x2, y2 = self.bound_number(x2, y2, frame)
-                    start = (x1, y1)
-                    end = (x2, y2)
-                    if not self.check_valid(k, start, end): 
-                        continue
+                assert ret
+                x1, y1, x2, y2 = self.draw_bbox(k)
+                x1, y1 = self.bound_number(x1, y1, frame)
+                x2, y2 = self.bound_number(x2, y2, frame)
+                start = (x1, y1)
+                end = (x2, y2)
+                if not self.check_valid(k, start, end): 
+                    continue
 
-                    if full:
-                        filename = os.path.join("dataset", \
-                            "S{}/Seq{}/imageSequence/full_video_{}/frame{:06}.jpg".format(self.S, self.Se, self.vid, k))
-                    else:
-                        filename = os.path.join("dataset", \
-                            "S{}/Seq{}/imageSequence/video_{}/frame{:06}.jpg".format(self.S, self.Se, self.vid, k))
+                if full:
+                    filename = os.path.join("dataset", \
+                        "S{}/Seq{}/imageSequence/full_video_{}/frame{:06}.jpg".format(self.S, self.Se, self.vid, k))
+                else:
+                    filename = os.path.join("dataset", \
+                        "S{}/Seq{}/imageSequence/video_{}/frame{:06}.jpg".format(self.S, self.Se, self.vid, k))
 
-                    if end[0]-start[0]==end[1]-start[1]:
-                        data[k] = {}
-                        if save_img:
-                            try:
-                                cropped_frame = frame[start[1]:end[1], start[0]:end[0]]
-                                if full:
-                                    cv.imwrite(filename, cv.resize(frame, (512,512), interpolation=cv.INTER_AREA))
-                                else:
-                                    cv.imwrite(filename, cropped_frame)
-                            except FileExistsError:
-                                pass
+                if end[0]-start[0] == end[1]-start[1]:
+                    data[k] = {}
+                    if save_img:
+                        try:
+                            cropped_frame = frame[start[1]:end[1], start[0]:end[0]]
+                            if full:
+                                cv.imwrite(filename, cv.resize(frame, (512,512), interpolation=cv.INTER_AREA))
+                            else:
+                                cv.imwrite(filename, cropped_frame)
+                        except FileExistsError:
+                            pass
 
-                        if save_npz:
-                            data[k]["directory"] = filename
-                            data[k]["bbox_start"] = start
-                            data[k]["bbox_end"] = end
-                            data[k]["pts_2d"] = self.imgPoint.reshape(-1,2)
-                            data[k]["pts_3d"] = self.objPoint.reshape(-1,3)
-
+                    if save_npz:
+                        data[k]["directory"] = filename
+                        data[k]["bbox_start"] = start
+                        data[k]["bbox_end"] = end
+                        data[k]["pts_2d"] = self.imgPoint.reshape(-1,2)
+                        data[k]["pts_3d"] = self.objPoint.reshape(-1,3)
             break
         if full:
             np.savez_compressed("dataset/S{}/Seq{}/imageSequence/full_video_{}".format(self.S,self.Se,self.vid), data)
         else:
             np.savez_compressed("dataset/S{}/Seq{}/imageSequence/video_{}".format(self.S,self.Se,self.vid), data)
-
         cap.release()
 
 
@@ -238,8 +200,10 @@ def save_frame(human):
             v = Video(human, seq, vid)
             v.save_cropped(False, True, False)
 
+
 def merge_npz(human):
     merge_data = []
+    print("Processing...")
     for s in [1,2]:
         for k in [0,1,2,4,5,6,7,8]:
             npz = "./dataset/S{}/Seq{}/imageSequence/video_{}.npz".format(human,s,k)
