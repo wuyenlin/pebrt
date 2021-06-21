823fc96b2cc7d1d7bbd464298bb7696d20f96857
wuyenlin <finne0831@gmail.com>
2021-06-21 15:20:01 +0800

    allow vectorize to work on h36m

--- common/human.py
+++ common/human.py
@@ -57,47 +57,47 @@ class Human:
         }
 
         self.constraints = {
-            'lower_spine': ((-0.61,0.61), (-0.52,0.52), (-0.52,1.31)),
-            'upper_spine': ((0,0), (0,0), (0,1.66)),
-            'neck': ((0,0), (0,0), (0,1.22)),
-            'head': ((-0.61,0.61), (-1.22,1.22), (-0.96,1.39)),
+            "lower_spine": ((-0.61,0.61), (-0.52,0.52), (-0.52,1.31)),
+            "upper_spine": ((0,0), (0,0), (0,1.66)),
+            "neck": ((0,0), (0,0), (0,1.22)),
+            "head": ((-0.61,0.61), (-1.22,1.22), (-0.96,1.39)),
 
-            'l_clavicle': ((0,0), (0,0), (0,0)), #4
-            'l_upper_arm': ((-1.57,2.28), (-0.707,2.27), (-1.57,3.14)),
-            'l_lower_arm': ((0,0), (0,2.62), (0,0)),
-            'r_clavicle': ((0,0), (0,0), (0,0)),
-            'r_upper_arm': ((-2.28,1.57), (-2.27,0.707), (-1.57,3.14)),
-            'r_lower_arm': ((0,0), (-2.62,0), (0,0)),
+            "l_clavicle": ((0,0), (0,0), (0,0)), #4
+            "l_upper_arm": ((-1.57,2.28), (-0.707,2.27), (-1.57,3.14)),
+            "l_lower_arm": ((0,0), (0,2.62), (0,0)),
+            "r_clavicle": ((0,0), (0,0), (0,0)),
+            "r_upper_arm": ((-2.28,1.57), (-2.27,0.707), (-1.57,3.14)),
+            "r_lower_arm": ((0,0), (-2.62,0), (0,0)),
 
-            'l_hip': ((0,0), (0,0), (0,0)), #10
-            'l_thigh': ((-0.87,0.35), (-0.785,0.785), (-2.09,0.52)),
-            'l_calf': ((0,0), (0,0), (0,2.79)),
-            'r_hip': ((0,0), (0,0), (0,0)),
-            'r_thigh': ((-0.35,0.87), (-0.785,0.785), (-0.52,2.09)),
-            'r_calf': ((0,0), (0,0), (0,2.79)),
+            "l_hip": ((0,0), (0,0), (0,0)), #10
+            "l_thigh": ((-0.87,0.35), (-0.785,0.785), (-2.09,0.52)),
+            "l_calf": ((0,0), (0,0), (0,2.79)),
+            "r_hip": ((0,0), (0,0), (0,0)),
+            "r_thigh": ((-0.35,0.87), (-0.785,0.785), (-2.09,0.52)),
+            "r_calf": ((0,0), (0,0), (0,2.79)),
         }
 
 
     def init_bones(self):
         self.bones = {
-            'lower_spine': torch.tensor([0, -self.lower_spine, 0]),
-            'upper_spine': torch.tensor([0, -self.upper_spine, 0]),
-            'neck': torch.tensor([0, -self.neck, 0]),
-            'head': torch.tensor([0, -self.half_face, 0]),
+            "lower_spine": torch.tensor([0, -self.lower_spine, 0]),
+            "upper_spine": torch.tensor([0, -self.upper_spine, 0]),
+            "neck": torch.tensor([0, -self.neck, 0]),
+            "head": torch.tensor([0, -self.half_face, 0]),
 
-            'l_clavicle': torch.tensor([self.clavicle, 0, 0]),
-            'l_upper_arm': torch.tensor([self.upper_arm, 0, 0]),
-            'l_lower_arm': torch.tensor([self.lower_arm, 0, 0]),
-            'r_clavicle': torch.tensor([-self.clavicle, 0, 0]),
-            'r_upper_arm': torch.tensor([-self.upper_arm, 0, 0]),
-            'r_lower_arm': torch.tensor([-self.lower_arm, 0, 0]),
+            "l_clavicle": torch.tensor([self.clavicle, 0, 0]),
+            "l_upper_arm": torch.tensor([self.upper_arm, 0, 0]),
+            "l_lower_arm": torch.tensor([self.lower_arm, 0, 0]),
+            "r_clavicle": torch.tensor([-self.clavicle, 0, 0]),
+            "r_upper_arm": torch.tensor([-self.upper_arm, 0, 0]),
+            "r_lower_arm": torch.tensor([-self.lower_arm, 0, 0]),
 
-            'l_hip': torch.tensor([self.pelvis/2, 0, 0]),
-            'l_thigh': torch.tensor([0, self.thigh, 0]),
-            'l_calf': torch.tensor([0, self.calf, 0]),
-            'r_hip': torch.tensor([-self.pelvis/2, 0, 0]),
-            'r_thigh': torch.tensor([0, self.thigh, 0]),
-            'r_calf': torch.tensor([0, self.calf, 0])
+            "l_hip": torch.tensor([self.pelvis/2, 0, 0]),
+            "l_thigh": torch.tensor([0, self.thigh, 0]),
+            "l_calf": torch.tensor([0, self.calf, 0]),
+            "r_hip": torch.tensor([-self.pelvis/2, 0, 0]),
+            "r_thigh": torch.tensor([0, self.thigh, 0]),
+            "r_calf": torch.tensor([0, self.calf, 0])
         }
         self.bones = { bone: self.bones[bone].to(self.device) for bone in self.bones.keys() }
         
@@ -107,7 +107,7 @@ class Human:
         for i in range(3):
             low = self.constraints[bone][i][0]
             high = self.constraints[bone][i][1]
-            if high != low and high != 0:
+            if high != low:
                 if angles[i] < low:
                     angles[i] = low
                     punish_w += 1.0
@@ -124,7 +124,7 @@ class Human:
         import torch.nn.functional as f
         absolute_angles = rot_to_euler(np.array(R).reshape(3,-1))
         if parent is not None:
-            parent_angles = rot_to_euler(parent.detach().numpy())
+            parent_angles = rot_to_euler(parent.detach().cpu().numpy())
             child_angles = absolute_angles
             relative_angles = child_angles - parent_angles
             aug_angles, punish_w = self.check_range(bone, relative_angles)
@@ -132,7 +132,7 @@ class Human:
         else:
             aug_angles, punish_w = self.check_range(bone, absolute_angles)
             R = rot(aug_angles)
-        return f.normalize(R.to(torch.float32)), punish_w
+        return f.normalize(R.to(torch.float32).to(self.device)), punish_w
 
 
     def sort_rot(self, elem):
@@ -143,7 +143,7 @@ class Human:
         elem = elem.flatten()
         self.rot_mat, self.punish_list = {}, []
         for k, bone in enumerate(self.constraints.keys()):
-            R = elem[9*k:9*(k+1)]
+            R = elem[9*k:9*(k+1)].detach().numpy()
             if bone in self.child.keys():
                 parent = self.child[bone]
                 self.rot_mat[bone], punish_w = self.check_constraints(bone, R, self.rot_mat[parent])
@@ -170,24 +170,24 @@ class Human:
         self.update_bones(elem)
 
         root = self.root
-        lower_spine = self.bones['lower_spine']
-        neck = self.bones['upper_spine'] + lower_spine
-        chin = self.bones['neck'] + neck
-        nose = self.bones['head'] + chin
+        lower_spine = self.bones["lower_spine"]
+        neck = self.bones["upper_spine"] + lower_spine
+        chin = self.bones["neck"] + neck
+        nose = self.bones["head"] + chin
 
-        l_shoulder = self.bones['l_clavicle'] + neck
-        l_elbow = self.bones['l_upper_arm'] + l_shoulder
-        l_wrist = self.bones['l_lower_arm'] + l_elbow
-        r_shoulder = self.bones['r_clavicle'] + neck
-        r_elbow = self.bones['r_upper_arm'] + r_shoulder
-        r_wrist = self.bones['r_lower_arm'] + r_elbow
+        l_shoulder = self.bones["l_clavicle"] + neck
+        l_elbow = self.bones["l_upper_arm"] + l_shoulder
+        l_wrist = self.bones["l_lower_arm"] + l_elbow
+        r_shoulder = self.bones["r_clavicle"] + neck
+        r_elbow = self.bones["r_upper_arm"] + r_shoulder
+        r_wrist = self.bones["r_lower_arm"] + r_elbow
 
-        l_hip = self.bones['l_hip']
-        l_knee = self.bones['l_thigh'] + l_hip
-        l_ankle = self.bones['l_calf'] + l_knee
-        r_hip = self.bones['r_hip']
-        r_knee = self.bones['r_thigh'] + r_hip
-        r_ankle = self.bones['r_calf'] + r_knee
+        l_hip = self.bones["l_hip"]
+        l_knee = self.bones["l_thigh"] + l_hip
+        l_ankle = self.bones["l_calf"] + l_knee
+        r_hip = self.bones["r_hip"]
+        r_knee = self.bones["r_thigh"] + r_hip
+        r_ankle = self.bones["r_calf"] + r_knee
 
         self.model = torch.stack((neck, lower_spine, root, chin, nose,
                 l_shoulder, l_elbow, l_wrist, r_shoulder, r_elbow, r_wrist,
@@ -195,18 +195,30 @@ class Human:
         return self.model
 
 
-def vectorize(gt_3d) -> torch.tensor:
+def vectorize(gt_3d, dataset="mpi") -> torch.tensor:
     """
     process gt_3d (17,3) into a (16,4) that contains bone vector and length
     :return bone_info: [unit bone vector (,3) + bone length (,1)]
     """
-    indices = (
-        (2,1), (1,0), (0,3), (3,4),  # spine + head
-        (0,5), (5,6), (6,7), 
-        (0,8), (8,9), (9,10), # arms
-        (2,14), (11,12), (12,13),
-        (2,11), (14,15), (15,16), # legs
-    )
+    if dataset == "mpi":
+        indices = (
+            (2,1), (1,0), (0,3), (3,4),  # spine + head
+            (0,5), (5,6), (6,7), 
+            (0,8), (8,9), (9,10), # arms
+            (2,11), (11,12), (12,13),
+            (2,14), (14,15), (15,16), # legs
+        )
+    elif dataset == "h36m":
+        indices = (
+            (0,7), (7,8), (8,9), (9,10),  # spine + head
+            (8,11), (11,12), (12,13), 
+            (8,14), (14,15), (15,16), # arms
+            (0,4), (4,5), (5,6),
+            (0,1), (1,2), (2,3), # legs
+        )
+    else:
+        print("Unrecognized dataset name.")
+
     num_bones = len(indices)
     try:
         gt_3d_tensor = torch.from_numpy(gt_3d)
@@ -234,9 +246,9 @@ def vis_model(model):
     )
     import matplotlib.pyplot as plt
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    ax = fig.add_subplot(111, projection="3d")
     for p in model:
-        ax.scatter(p[0], p[1], p[2], c='r')
+        ax.scatter(p[0], p[1], p[2], c="r")
 
     for index in indices:
         xS = (model[index[0]][0], model[index[1]][0])
@@ -256,8 +268,12 @@ def rand_pose():
     h = Human(1.8, "cpu")
     euler = (0,0,0)
     a = rot(euler).flatten().repeat(16)
-    k = 3
-    a[9*k:9*k+9] = rot((0,0,-0.96)).flatten()
+    # k = 5
+    # a[9*k:9*k+9] = rot((0,1,0)).flatten()
+    k = 8
+    a[9*k:9*k+9] = rot((0,-1,0)).flatten()
+    # k = 14
+    # a[9*k:9*k+9] = rot((0,0,0.5)).flatten()
     model = h.update_pose(a)
     print(h.punish_list)
     vis_model(model)
