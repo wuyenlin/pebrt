dd5097365d15eae011d613e82afba67320e14a38
wuyenlin <finne0831@gmail.com>
2021-06-22 09:03:26 +0800

    viz h36m on peltra

--- plot.py
+++ plot.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import numpy as np
 import torch
 from torchvision import transforms
 from PIL import Image
@@ -18,19 +17,29 @@ transforms = transforms.Compose([
 ]) 
 
 
-bones = (
-    (2,1), (1,0), (0,3), (3,4),  # spine + head
-    (0,5), (5,6), (6,7), 
-    (0,8), (8,9), (9,10), # arms
-    (2,14), (11,12), (12,13),
-    (2,11), (14,15), (15,16), # legs
-)
+bones = {
+    "mpi": (
+        (2,1), (1,0), (0,3), (3,4),  # spine + head
+        (0,5), (5,6), (6,7), 
+        (0,8), (8,9), (9,10), # arms
+        (2,14), (11,12), (12,13),
+        (2,11), (14,15), (15,16) # legs
+    ),
+    "h36m": (
+        (0,7), (7,8), (8,9), (9,10),  # spine + head
+        (8,14), (14,15), (15,16), 
+        (8,11), (11,12), (12,13), # arms
+        (0,1), (1,2), (2,3),
+        (0,4), (4,5), (5,6) # legs
+    )
+}
 
 
-def plot3d(ax, bones, output):
+def plot3d(ax, bones, output, dataset="mpi"):
     for p in output:
-        ax.scatter(p[0], p[1], p[2], c='r', alpha=0.5)
+        ax.scatter(p[0], p[1], p[2], c="r", alpha=0.5)
 
+    bones = bones[dataset]
     for index in bones:
         xS = (output[index[0]][0],output[index[1]][0])
         yS = (output[index[0]][1],output[index[1]][1])
@@ -45,8 +54,9 @@ def plot3d(ax, bones, output):
     ax.set_zlabel("Z")
 
 
-def viz(savefig=False):
-    train_npz = "./dataset/S1/Seq1/imageSequence/S1.npz"
+def viz(dataset="mpi", savefig=False):
+    # train_npz = "./dataset/S1/Seq1/imageSequence/S1.npz"
+    train_npz = "./h36m/data_h36m_frame_S1.npz"
     train_dataset = Data(train_npz, transforms, True)
     trainloader = DataLoader(train_dataset, batch_size=4, 
                         shuffle=True, num_workers=8, drop_last=True)
@@ -56,8 +66,8 @@ def viz(savefig=False):
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     net = PELTRA(device)
-    # net.load_state_dict(torch.load('../archive/peltra/ft_1_cam.bin')['model'])
-    net.load_state_dict(torch.load('./peltra/ft_1_zero.bin')['model'])
+    # net.load_state_dict(torch.load("./peltra/ft_1_zero.bin")["model"])
+    net.load_state_dict(torch.load("./peltra/ft_1_h36m.bin")["model"])
     net = net.cuda()
     net.eval()
 
@@ -71,12 +81,13 @@ def viz(savefig=False):
         h = Human(1.8, "cpu")
         output = h.update_pose(output.detach().numpy())
 
-        ax = fig.add_subplot(2, 4, k+4, projection='3d')
-        plot3d(ax, bones, output)
+        ax = fig.add_subplot(2, 4, k+4, projection="3d")
+        plot3d(ax, bones, output, dataset)
 
     plt.show()
+
     if savefig:
-        plt.savefig('./checkpoint/this.svg', format='svg', dpi=1200)
+        plt.savefig("./checkpoint/this.svg", format="svg", dpi=1200)
 
 
 if __name__ == "__main__":
