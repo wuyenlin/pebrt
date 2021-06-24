319f689afad639748ecd1767503a7ae81fdb8146
wuyenlin <finne0831@gmail.com>
2021-06-24 17:26:55 +0800

    set training and testing subjects and moved funcs to misc

--- common/dataloader.py
+++ common/dataloader.py
@@ -1,13 +1,7 @@
 import numpy as np
 import cv2 as cv
 from PIL import Image
-from torchvision import transforms
 from common.human import *
-
-
-def collate_fn(batch):
-    batch = list(filter(lambda x: x is not None, batch))
-    return torch.utils.data.dataloader.default_collate(batch)
 
 
 class Data:
@@ -49,6 +43,7 @@ class Data:
                     assert gt_2d.shape == (17,2) and gt_3d.shape == (17,3)
                     self.gt_pts2d.append(gt_2d)
                     self.gt_pts3d.append(gt_3d)
+                    self.gt_vecs3d.append(convert_gt(gt_3d, t_info, "h36m"))
                     self.img_path.append(frames[f]["directory"])
 
         else:
@@ -60,16 +55,16 @@ class Data:
 
             for vid in vid_list:
                 for frame in data[vid].keys():
-                    # pts_2d = data[vid][frame]["positions_2d"]
-                    pts_2d = data[vid][frame]["pts_2d"]
+                    pts_2d = data[vid][frame]["positions_2d"]
                     gt_2d = self.zero_center(self.remove_joints(pts_2d))
 
-                    pts_3d = data[vid][frame]["pts_3d"]
+                    pts_3d = data[vid][frame]["positions_3d"]
                     cam_3d = self.to_camera_coordinate(pts_2d, pts_3d, vid)
                     gt_3d = self.zero_center(cam_3d)/1000
 
                     self.gt_pts2d.append(gt_2d)
                     self.gt_pts3d.append(gt_3d)
+                    self.gt_vecs3d.append((convert_gt(gt_3d, t_info)))
                     self.img_path.append(data[vid][frame]["directory"])
 
 
@@ -79,10 +74,10 @@ class Data:
             img = Image.open(img_path)
             img = self.transforms(img)
             kpts_2d = self.gt_pts2d[index]
-            kpts_3d = self.gt_pts3d[index]
+            vecs_3d = self.gt_vecs3d[index]
         except:
             return None
-        return img_path, img, kpts_2d, kpts_3d
+        return img_path, img, kpts_2d, vecs_3d
         
 
     def __len__(self):
