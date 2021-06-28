854bfcd99b2020385dceda09992af0d8c9d75d76
wuyenlin <finne0831@gmail.com>
2021-06-28 09:56:40 +0800

    added eval mode

--- this.sh
+++ this.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-CMD="python3 main.py "
-#CMD="python3 finetune.py  --resume ./world_checkpoint/ft_2.bin"
+#CMD="python3 main.py"
+CMD="python3 main.py --resume ./petr/epoch_20_h36m.bin"
+#CMD="python3 main.py --eval --resume ./petr/epoch_10_h36m.bin"
+# CMD="python3 main.py --eval --resume ./checkpoint/ft_5.bin"
+# CMD="python3 main.py  --eval --resume ./petr/ft_5_h36m.bin"
 echo $CMD
 $CMD
