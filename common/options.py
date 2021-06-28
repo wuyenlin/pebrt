d6c8d444408bf6b34c2bf3a013491a31aec17aac
wuyenlin <finne0831@gmail.com>
2021-06-28 10:59:37 +0800

    using h36m to train

--- common/options.py
+++ common/options.py
@@ -6,7 +6,7 @@ def args_parser():
     
     # Hyperparameters
     parser.add_argument('--start_epoch', type=int, default=0)
-    parser.add_argument('--epoch', type=int, default=51)
+    parser.add_argument('--epoch', type=int, default=81)
     parser.add_argument('--bs', type=int, default=2)
     parser.add_argument('--lr', type=float, default=1e-04)
     parser.add_argument('--lr_backbone', type=float, default=0)
@@ -21,7 +21,8 @@ def args_parser():
     parser.add_argument('--num_workers', default=1, type=int)
     parser.add_argument('--eval', action='store_true')
     parser.add_argument('--export_training_curves', action='store_true', help="Save train/val curves in .png file")
-    parser.add_argument('--dataset', type=str, default="./dataset/S1/Seq1/imageSequence/S1.npz")
+    # parser.add_argument('--dataset', type=str, default="./dataset/S1/Seq1/imageSequence/S1.npz")
+    parser.add_argument('--dataset', type=str, default="./h36m/data_h36m_frame_all.npz")
     parser.add_argument('--device', default="cuda", help="device used")
     parser.add_argument('--resume', type=str, default=None, help="Loading model checkpoint")
     parser.add_argument('--distributed', action='store_true')
