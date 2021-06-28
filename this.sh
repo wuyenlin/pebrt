5b2e0c6a4285030282d51a49320cfb19d4bce76e
wuyenlin <finne0831@gmail.com>
2021-06-28 09:58:55 +0800

    eval on latest model

--- this.sh
+++ this.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #CMD="python3 main.py"
-CMD="python3 main.py --resume ./petr/epoch_20_h36m.bin"
-#CMD="python3 main.py --eval --resume ./petr/epoch_10_h36m.bin"
+#CMD="python3 main.py --resume ./petr/epoch_20_h36m.bin"
+CMD="python3 main.py --eval --resume ./petr/epoch_50_h36m.bin"
 # CMD="python3 main.py --eval --resume ./checkpoint/ft_5.bin"
 # CMD="python3 main.py  --eval --resume ./petr/ft_5_h36m.bin"
 echo $CMD
