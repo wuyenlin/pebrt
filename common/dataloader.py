0d9f6db5ae93807cfe918260fc67ca45342c903a
wuyenlin <finne0831@gmail.com>
2021-07-12 08:34:51 +0200

    remap h36m

--- common/dataloader.py
+++ common/dataloader.py
@@ -17,6 +17,7 @@ class Data:
         self.transforms = transforms
 
         data = np.load(npz_path, allow_pickle=True)
+        num_frame = 0
         if "h36m" in npz_path:
             print("INFO: Using Human3.6M dataset.")
             subject = {
@@ -37,7 +38,9 @@ class Data:
             import random
             for act in to_load:
                 frames = data[act].flatten()[0]
-                reduced = random.sample(list(frames), int(len(frames)*0.5))
+                reduced = random.sample(list(frames), int(len(frames))) \
+                    if action is None else random.sample(list(frames), int(len(frames)))
+                num_frame += len(reduced)
                 for f in reduced:
                     gt_2d = self.zero_center(frames[f]["positions_2d"], "h36m")
                     gt_3d = self.zero_center(self.remove_joints( \
@@ -59,6 +62,7 @@ class Data:
                 vid_list = np.arange(6,8)
 
             for vid in vid_list:
+                num_frame += len(data[vid].keys())
                 for frame in data[vid].keys():
                     # pts_2d = data[vid][frame]["positions_2d"]
                     pts_2d = data[vid][frame]["pts_2d"]
@@ -71,6 +75,8 @@ class Data:
                     self.gt_pts2d.append(gt_2d)
                     self.gt_pts3d.append(gt_3d)
                     self.img_path.append(data[vid][frame]["directory"])
+
+        print("INFO: Using ", num_frame, " frames")
 
 
     def __getitem__(self, index):
@@ -106,6 +112,7 @@ class Data:
             keep = [0,1,2,3,6,7,8,12,13,14,15,17,18,19,25,26,27]
             for row in range(17):
                 new_skel[row, :] = kpts[keep[row], :]
+            new_skel = self.remap_h36m(new_skel)
         else:
             print("Unrecognized dataset name.")
         return new_skel
@@ -148,3 +155,19 @@ class Data:
             return cam - cam[0,:]
         else:
             print("Unrecognized dataset name.")
+
+    def remap_h36m(self, h36m_joints):
+        """
+        convert Human3.6M joint order
+        to normal MPI-INF-3DHP one
+        """
+        mpi_joints = np.zeros_like(h36m_joints)
+        replace_list = (
+            (0,8), (1,7), (2,0), (3,9), (4,10),
+            (5,11), (6,12), (7,13), (8,14), (9,15), (10,16),
+            (11,4), (12,5), (13,6), (14,1), (15,2), (16,3)
+        )
+        for joint in replace_list:
+            mpi_joints[joint[0]] = h36m_joints[joint[1]]
+
+        return mpi_joints
