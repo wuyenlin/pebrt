82421adc07b542c99c7282dc50bff4338fe700eb
wuyenlin <finne0831@gmail.com>
2021-07-12 09:23:00 +0200

    added cmd to run sli

--- this.sh
+++ this.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #CMD="python3 main.py"
 #CMD="python3 main.py --resume ./petr/epoch_20_h36m.bin"
-CMD="python3 main.py --eval --resume ./petr/epoch_50_h36m.bin"
-# CMD="python3 main.py --eval --resume ./checkpoint/ft_5.bin"
-# CMD="python3 main.py  --eval --resume ./petr/ft_5_h36m.bin"
+CMD='python3 -m torch.distributed.launch --nproc_per_node=2 --nnodes=1 main.py'
 echo $CMD
 $CMD
+
