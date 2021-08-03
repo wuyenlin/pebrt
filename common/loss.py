3890a4868142c28ea29cad64d275d7ea4dab7117
Yen-Lin Wu <finne0831@gmail.com>
2021-08-03 10:49:45 +0200

    added pck and auc

--- common/loss.py
+++ common/loss.py
@@ -1,5 +1,37 @@
+import cmath
 import torch
-from common.misc import *
+from common.human import *
+
+
+def pck(predicted, target):
+    """
+    Modified from sunnychencool/Anatomy3D
+    (https://github.com/sunnychencool/Anatomy3D/blob/master/common/loss.py)
+    """
+    assert predicted.shape == target.shape
+    dis = torch.norm(predicted - target, dim=len(target.shape)-1)
+    # threshold
+    t = torch.Tensor([0.15]).cuda() if torch.cuda.is_available() else torch.Tensor([0.15])
+    out = (dis < t).float() * 1
+    return out.sum()/14.0
+
+
+def auc(predicted, target):
+    """
+    Modified from sunnychencool/Anatomy3D
+    (https://github.com/sunnychencool/Anatomy3D/blob/master/common/loss.py)
+    """
+    assert predicted.shape == target.shape
+    dis = torch.norm(predicted - target, dim=len(target.shape)-1)
+    outall = 0
+    for i in range(150):
+        # threshold
+        t = torch.Tensor([float(i)/1000]).cuda() if torch.cuda.is_available() \
+            else torch.Tensor([float(i)/1000])
+        out = (dis < t).float() * 1
+        outall+=out.sum()/14.0
+    outall = outall/150
+    return outall
 
 
 def mpjpe(predicted, target):
@@ -16,82 +48,61 @@ def mpjpe(predicted, target):
     return torch.mean(torch.norm(predicted - target, dim=len(target.shape)-1))
 
 
-def punish(predicted, bone1, bone2, weights, thres=0.1):
+
+def is_so(M):
+    det = cmath.isclose(torch.linalg.det(M), 1, rel_tol=1e-03)
+    orth = cmath.isclose(torch.linalg.det(M@M.T), 1, rel_tol=1e-03)
+    return 1 if orth and det else 2
+
+
+def maev(predicted, target, w_kc=None):
     """
-    Verifies that symmetric bones have the same length.
-    If the predicted skeleton shows different bone lengths on a pair of bones,
-    one of their respective joints will be weighted 2 (instead of 1) in loss function.
-    # TODO: add BONE collision (see if 2 lines/volumes have intersection)
-
-    :param bone1:
-    :param bone2:
-    :return: a weight matrix of shape (bs, 17)
+    MAEV: Mean Absolute Error of Vectors
+    :param predicted: (bs,16,9) tensor
+    :param target:  (bs,16,9) tensor
+    :param w_kc: weight of kinematic constraints
+    average error of 16 bones
     """
-
-# the commented part is an easier explanation of this function, 
-# but training becomes slower as bs increases
-
-    # for bs in range(predicted.shape[0]):
-    #     len_bone1 = torch.norm(predicted[bs, bone1[0], :]-predicted[bs, bone1[1], :])
-    #     len_bone2 = torch.norm(predicted[bs, bone2[0], :]-predicted[bs, bone2[1], :])
-    #     if not math.isclose(len_bone1, len_bone2, abs_tol=1e-02):
-    #         weights[bs, bone1[0]] = 2
-    #         weights[bs, bone2[0]] = 2
-
-    len1 = torch.norm(predicted[:, bone1[0], :]-predicted[:, bone1[1], :], dim=1)
-    len2 = torch.norm(predicted[:, bone2[0], :]-predicted[:, bone2[1], :], dim=1)
-    diff = abs(len2-len1) > thres
-    diff = diff.double() + 1
-    weights[:,bone1[0]], weights[:,bone1[1]] = diff, diff
-    weights[:,bone2[0]], weights[:,bone2[1]] = diff, diff
-
-    return weights
-
-
-def joint_collision(predicted, target, weight, thres=0.1):
-    """
-    verify whether predicted and target joints lie within a given threshold
-    if True -> collision -> punish
-
-    :return: a weight matrix of shape (bs, 17)
-    """
-    bs = predicted.shape[0]
-    diff = torch.linalg.norm(predicted - target, dim=2) < thres
-    diff = diff.double() + 1
-    weight *= diff
-
-    return weight
-
-
-def anth_mpjpe(predicted, target):
-    """
-    Added own implementation of weighted MPJPE by ensuring:
-    1. symmetric bones have the same length
-    2. wrists and ankles are correctly detected
-    3. joint collision/occlusion are punished
-    """
-    assert predicted.shape == target.shape, "{}, {}".format(predicted.shape, target.shape)
-    bs = predicted.shape[0]
-    w = torch.ones(bs, predicted.shape[1]) # (bs,17)
-
+    bs, num_bones = predicted.size(0), predicted.size(1)
+    predicted = predicted.view(bs,num_bones,3,3)
+    target = target.view(bs,num_bones,3,3)
+    w_orth = torch.ones(target.shape[:2])
+    for b in range(bs):
+        for bone in range(num_bones):
+            M = predicted[b,bone]
+            w_orth[b,bone] = is_so(M)
     if torch.cuda.is_available():
         predicted = predicted.cuda()
         target = target.cuda()
-        w = w.cuda()
-    
-    bones = (((5,6),(8,9)),
-             ((6,7),(9,10)),
-             ((11,12),(14,15)),
-             ((12,13),(15,16)))
+        w_orth = w_orth.cuda()
+        w_kc = w_kc.cuda() if w_kc is not None else w_kc
+    aev = torch.norm(torch.norm(predicted - target, dim=len(target.shape)-2), dim=len(target.shape)-2)
+    maev = torch.mean(aev*w_kc) if w_kc is not None else torch.mean(aev)
+    return maev
 
-    # 1. symmetric bones have same length, else punish
-    for bone in bones:
-        w = punish(predicted, bone[0], bone[1], w)
 
-    # 2. focus on correct prediction of wrists and ankles
-    w[:,7], w[:,10], w[:,13], w[:,16] = 2, 2, 2, 2
+def mpbve(predicted, target, w_kc):
+    """
+    MPBVE - Mean Per Bone Vector Error
+    Novel pose accuracy evaluation metric-
+    Normalize each bone to 1m and calculate the mean L2 norms
+    :param predicted: (bs,16,9) tensor
+    :param target:  (bs,16,9) tensor
+    """
+    if torch.cuda.is_available():
+        predicted = predicted.cuda()
+        target = target.cuda()
+    bs, num_bones = predicted.size(0), predicted.size(1)
 
-    # 3. joint collision
-    w = joint_collision(predicted, target, w)
+    pred_info = torch.zeros(bs, num_bones, 3)
+    tar_info = torch.zeros(bs, num_bones, 3)
 
-    return torch.mean(w * torch.norm(predicted - target, dim=len(target.shape)-1))
+    for b in range(bs):
+        pred = Human(1.8, "cpu")
+        pred_model = pred.update_pose(predicted[b])
+        pred_info[b,:] = vectorize(pred_model)[:,:3]
+        tar = Human(1.8, "cpu")
+        tar_model = tar.update_pose(target[b])
+        tar_info[b,:] = vectorize(tar_model)[:,:3]
+    mpbve = torch.mean(torch.norm(pred_info - tar_info, dim=len(tar_info.shape)-1))
+    return mpbve
