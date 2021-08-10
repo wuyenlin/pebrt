3ca3ca6c6799369de9dbb7453c5a02da4bcbef2c
Yen-Lin Wu <finne0831@gmail.com>
2021-08-10 16:14:24 +0200

    cleaned up files

--- eval.sh
+++
@@ -1,1 +0,0 @@
-python3 main.py --eval --checkpoint ./petr/all_2_lay_epoch_30_h36m.bin
--- this.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#CMD="python3 main.py"
-#CMD="python3 main.py --resume ./petr/epoch_20_h36m.bin"
-CMD='python3 -m torch.distributed.launch --nproc_per_node=2 --nnodes=1 main.py'
-echo $CMD
-$CMD
-
